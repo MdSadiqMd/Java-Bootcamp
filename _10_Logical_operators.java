ddf96f9833f149844dc91df23365b63d0660faeb
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-02-10 19:17:53 +0530

    Update _10_Logical_operators.java

--- _10_Logical_operators.java
+++ _10_Logical_operators.java
@@ -26,6 +26,19 @@ public class _10_Logical_operators {
         //using logical NOT operator it will print the reverse value of input
 
         System.out.println("logical not of a :"+!a);
+        
+         //Ternary operator
+        int c=4;
+        int d=5;
+        int e=5;
+        int result=(c>d)?c:d; //here if a>b then it will print a if not it will print b
+        String result1=(c>d)?"c is greater":"d is greater"; //*** should give string before it
+        System.out.println(result);
+        System.out.println(result1);
+
+        //if,else if and else in ternary operator
+        int result2=(c<d)?((d<e)?c:e):(d<e)?d:e;
+        System.out.println(result2);
 
     }
 
