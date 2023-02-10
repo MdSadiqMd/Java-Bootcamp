e6a87621042f5a9fe599fffe9e996c9ff32648dc
MdSadiqMd <mohammadsadiq4950@gmail.com>
2023-02-10 19:33:34 +0530

    commited

--- _2_All_Data_Types.java
+++ _2_All_Data_Types.java
@@ -4,7 +4,7 @@ public class _2_All_Data_Types {
         int age2 = 56;
         short age3 = 87; //these three have some range will lie in it
         long age4 = 23457653786978L; //here we need to write capital L at last to specify it is long
-        char ch = 'a'; //here need to write chracter in quotations
+        char ch = 'a'; //here need to write character in quotations
         float f=345.545f; //here we need to write f at last to specify it is a float value other wise it will become double
         double d1=345.45D; //here we need to write capital D at last to specify it is an double value
         System.out.println(age);
--- _9_If_else_statement.java
+++ _9_If_else_statement.java
@@ -24,5 +24,19 @@ public class _9_If_else_statement {
         else{
             System.out.println("the person is not eligible to vote - boolean output");
         }
+
+        //Ternary operator
+        int c=4;
+        int d=5;
+        int e=5;
+        int result=(c>d)?c:d; //here if a>b then it will print a if not it will print b
+        String result1=(c>d)?"c is greater":"d is greater"; //*** should give string before it
+        System.out.println(result);
+        System.out.println(result1);
+
+        //if,else if and else in ternary operator
+        int result2=(c<d)?((d<e)?c:e):(d<e)?d:e;
+        System.out.println(result2);
+
     }
 }
