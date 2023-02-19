87a0a0da71c92b902c50fe048ec5d082f60b6122
MdSadiqMd <mohammadsadiq4950@gmail.com>
2023-02-19 17:41:14 +0530

    committed

--- _4_User_input_in_java.java
+++ _4_User_input_in_java.java
@@ -11,7 +11,25 @@ public class _4_User_input_in_java {
         float b=sc.nextFloat(); //here we are entering the float dynamic memory capital F
         float sum=a+b;
         System.out.println("the sum is:"+sum);
+        System.out.println("enter string");
         String str =sc.nextLine(); // it is used to print the string if we not use Line word then only one letter will print
+        if(str.isEmpty()){
+            str=sc.nextLine();
+        }
         System.out.println(str);
+        //if we want to a string to integer,float,byte,long,double we use parse key word
+        /*
+        ex: i/p: "123"-->String
+            o/p: 123 -->integer
+            i/p: "sadiq"-->String
+            o/p: error
+         */
+        System.out.println("write an integer String to convert into integer,Float,byte,long,Double");
+        String str1=sc.nextLine();
+        System.out.println(Integer.parseInt(str1));
+        System.out.println(Float.parseFloat(str1));
+        System.out.println(Byte.parseByte(str1));
+        System.out.println(Long.parseLong(str1));
+        System.out.println(Double.parseDouble(str1));
     }
 }
