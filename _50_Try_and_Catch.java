edc142448729ce28184531f2b0f934ce255c9e8d
MdSadiqMd <mohammadsadiq4950@gmail.com>
2023-03-09 20:34:01 +0530

    committed

--- _50_Try_and_Catch.java
+++ _50_Try_and_Catch.java
@@ -1,3 +1,4 @@
+//Why do we need exception handling--> If there is no try and catch block while an exception occurred, the program gill terminate Exception handling-endured the smooth running of a program without program termination
 //we can write program try and catch without finally
 //we cannot write catch and finally without try
 //we can write try and finally without catch but, it's silly to do it
@@ -11,6 +12,9 @@ public class _50_Try_and_Catch {
             //*****here now we have given one error creating statement and other now mal statement and if we run it now then the successful statement will also not print so, we create some catch blocks that will catch specific exceptions and return the successful statements
             c=a/b; //successful statement
             System.out.println(values[10]); //error statement
+            //here we have a throw keyword for which we need to create the object to access it will return the object name as exception and redirect to catch blocks
+            Exception e = new Exception();
+            throw e;
         }
         //customizable errors
         catch(ArithmeticException e){
