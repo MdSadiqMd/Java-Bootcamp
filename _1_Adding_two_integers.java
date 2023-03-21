d5f39578ced4dce70ebd4feabe26aa56e6eae773
Md.Sadiq <mohammadsadiq4950gmail.com>
2023-03-21 22:03:02 +0530

    committed

--- _1_Adding_two_integers.java
+++ _1_Adding_two_integers.java
@@ -1,6 +1,7 @@
 /*
 Nothing Program execution
 .class file --> class loader sub system(Loading,Linking,Initialize) --> Run time area (method area,heap,java stack,PC registers,Native Stack Method) --> JVM(Interpreter,JIT compiler) --> operating system
+Every Java Program will have an inbuilt Main Thread
  */
 public class _1_Adding_two_integers {
     public static void main(String[] args) { //here we use static as to call main we need to create an object and to create the object we need to start execution to avoid this contradiction we use static keyword "here" refer static keyword file for clarity
--- _52_Main_Thread.java
+++ _52_Main_Thread.java
@@ -5,9 +5,13 @@ Here on device OS system is used for Context switching ,means it switch between 
 There are two types in Multitasking that are
 1.Process based Multitasking --> Executing Several tasks Simultaneously where each task is a separate Independent ***Process.Here the tasking is carried by OS(Windows)
 2.Thread Based Multitasking --> Executing Several tasks Simultaneously where each task is a separate Independent part of the ***Same Program,where each independent part is called a "Thread"
+***The Main Thread is Default Created Behind by JVM
  */
-public class _52_MultiThreading {
+public class _52_Main_Thread {
     public static void main(String[] args) {
-
+        System.out.println("Main thread");
+        String name=Thread.currentThread().getName(); //This Will give the name of the thread
+                                                     //This one statement shows that there will be a default inbuilt thread in java
+        System.out.println(name);
     }
 }
