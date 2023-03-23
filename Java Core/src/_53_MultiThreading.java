15ef5c91ad26b389015426d7d5b2ed398e8b94a4
Md.Sadiq <mohammadsadiq4950gmail.com>
2023-03-23 20:08:42 +0530

    committed

--- Java Core/src/_53_MultiThreading.java
+++ Java Core/src/_53_MultiThreading.java
@@ -1,8 +1,9 @@
 /*
-How Threading Works? --> There is an Interface called Runnable Interface Which has void run(); Function which is public and abstract
+How Threading Works ****Without Runnable Interface Acting Directly (refer 55)? --> There is an Interface called Runnable Interface Which has void run(); Function which is public and abstract
                      --> There is an inbuilt class by the name thread class in which many methods like join(),run(),sleep(),....
                      --> When we creating a class we write thread-name extends thread which activate the thread class which inherit thread methods to the created thread
                      --> We should not call run method we should call start method for output (t.start()) by which it hands over the thread to thread scheduler and if we call run then it will act as single thread program neglecting the default main thread
+                     Summary : Runnable Interface(public and Abstract) <-- Thread Class( which haves join(),Run(),sleep(),... Methods) <-- Extended by the thread we create
 There are two ways to create Threads
 1.By Extending Thread Class
 2.By Implementing Runnable Interface
--- Java Core/src/_55_Threading_in_Runnable_interface.java
+++ Java Core/src/_55_Threading_in_Runnable_interface.java
@@ -1,5 +1,13 @@
-//here in threading in output the output will come
-// as it like it will take preference as it like
+/*
+Here as we Knew before that Thread class will Inherit the Runnable Interface and Mythread will inherit thread Interface but here when we use Runnable interface the Mythread class will directly Inherit from Runnable Interface rather thread class in between
+Summary: Without Runnable Interface Acting Directly
+         Summary : Runnable Interface(public and Abstract) <-- Thread Class( which haves join(),Run(),sleep(),... Methods) <-- Extended by the thread we create
+         With Runnable Interface Acting Directly
+         Summary : Runnable Interface(public and Abstract) <-- Extended by the thread we create
+WHen we are using Runnable Directly we Use Implements runnable keyword After the Created Thread Name
+Using Runnable Interface Directly is More preferable as we can Extend and Implement many threads, but we can extend only one class
+The run Method is available in the thread class constructed Using a separate runnable object.Otherwise, this Method does nothing and returns.we can also run it multiple times
+ */
 class MythreadRunnable1 implements Runnable {
     public void run() {
         System.out.println("thread 1");
@@ -110,7 +118,7 @@ class MythreadRunnable4 implements Runnable{
     }
 }
 
-public class _60_Threading_in_Runnable_interface {
+public class _55_Threading_in_Runnable_interface {
     public static void main(String[] args) {
         MythreadRunnable1 t1= new MythreadRunnable1();
         Thread class1 = new Thread(t1);
@@ -121,13 +129,14 @@ public class _60_Threading_in_Runnable_interface {
         MythreadRunnable4 t4= new MythreadRunnable4();
         Thread class4 = new Thread(t4);
 
-        //if want to give priority to a thread that after executing if that thread i=only the other thread should run then we can use this priority
-        //class4.setPriority(MythreadRunnable4.MAX_PRIORITY); -->this will print the number first
-        //class1.setPriority(MythreadRunnable1.MIN_PRIORITY); -->this will print the number last
+        //If we want to give priority to a thread that after executing if that thread i only the other thread should run then we can use this priority
+        //class4.setPriority(MythreadRunnable4.MAX_PRIORITY); //-->this will print the number first
+        //class1.setPriority(MythreadRunnable1.MIN_PRIORITY); //-->this will print the number last
 
         class1.start();
         //here in this block we used join function which stops other to run until 1 had run and completed
-       /*try{
+       /*
+       try{
           class1.join();
         }
         catch(Exception e){
