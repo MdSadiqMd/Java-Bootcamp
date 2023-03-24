f045670f9cecc32d6a709a03f489e3b10db8a472
Md.Sadiq <mohammadsadiq4950gmail.com>
2023-03-24 19:54:19 +0530

    committed

--- Java Core/src/_59_Dead_Lock_in_MultiThreading.java
+++ Java Core/src/_59_Dead_Lock_in_MultiThreading.java
@@ -1,14 +1,14 @@
-class library implements Runnable{
+class BookHub implements Runnable{
     String book1=new String ("java");
     String book2=new String ("SQL");
     String book3=new String ("Spring");
     public void run() {
-
+        System.out.println("Nothing");
     }
 }
 public class _59_Dead_Lock_in_MultiThreading {
     public static void main(String[] args) {
-            library lb=new library();
+            BookHub lb=new BookHub();
             Thread t1=new Thread(lb);
             Thread t2=new Thread(lb);
     }
