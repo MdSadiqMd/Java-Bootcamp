7b3a0c083a329b041f200c1707c21353a0b423d0
Md.Sadiq <mohammadsadiq4950gmail.com>
2023-04-30 18:26:34 +0530

    committed

--- IO Operation/src/_8_Serialization.java
+++ IO Operation/src/_8_Serialization.java
@@ -8,8 +8,24 @@ To make data into sequence of bytes we use object output Stream
 To make the process more efficient and faster we have Buffer Output Stream and Buffer Input Stream
 To Reconstruct back the data of bytes we use Object Input Stream and File Input Stream
  */
+class Cricketer implements Serializable{
+    private String name;
+    private int age;
+    private int runs;
+    public Cricketer (String name,int age,int runs){
+        this.name=name;
+        this.age=age;
+        this.runs=runs;
+    }
+}
 public class _8_Serialization {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        Cricketer c=new Cricketer("virat",30,50000);
+        FileOutputStream fos=new FileOutputStream("_4_java.txt");
+        BufferedOutputStream bos=new BufferedOutputStream(fos);//this line makes code faster we can remove it if we want
+        ObjectOutputStream oos=new ObjectOutputStream(bos);//if we remove the above line replace bos with fos
+        oos.writeObject(c);//This c is converted into byte code which is not understandable by normal people and enhancing the security of the data of the code
+        oos.flush();
+        oos.close();
     }
 }
---
+++ IO Operation/src/_9_DeSerialization.java
@@ -0,0 +1,5 @@
+public class _9_DeSerialization {
+    public static void main(String[] args) {
+
+    }
+}
