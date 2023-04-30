fd81fe866d57e38509e5c189c97ac50395e935b6
Md.Sadiq <mohammadsadiq4950gmail.com>
2023-04-30 18:16:28 +0530

    committed

--- IO Operation/src/_8_Serialization.java
+++ IO Operation/src/_8_Serialization.java
@@ -2,6 +2,11 @@ import java.io.*;
 /*
 For Writing and Reading the data we have Buffer writer and reader and Print Writer
 But to store videos,Audios on to a file and making them persistent(storing permanently) we use Input Output Streams
+In serialization our main objective is security we store byte code in RAM that makes and to retrieve in normal form we use Deserialization
+The converted Bytes to transfer to a file we have FileOutput Stream
+To make data into sequence of bytes we use object output Stream
+To make the process more efficient and faster we have Buffer Output Stream and Buffer Input Stream
+To Reconstruct back the data of bytes we use Object Input Stream and File Input Stream
  */
 public class _8_Serialization {
     public static void main(String[] args) {
