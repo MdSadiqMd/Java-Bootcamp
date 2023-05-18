6b3cece4b6b254975b9a4486059cd0dc5631f61c
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-05-18 20:53:14 +0530

    Refactor --> Searching Algortihms

---
+++ Java DSA BootCamp/Searching Algorithm/src/_1_Linear_Search.java
@@ -0,0 +1,46 @@
+//Linear Search Means we Search Element by Element equating to the term need to be found
+//Can be used in sorted or unsorted array
+//But it's sucks if the Length of array is huge
+import java.util.Scanner;
+public class _1_Linear_Search {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       // int m=sc.nextInt();
+        System.out.println("Enter the Length of 1D Array");
+        int n=sc.nextInt();
+
+        //1D Array
+        System.out.println("Enter the Values of 1D Array");
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the element needed to be Searched");
+        int k=sc.nextInt();
+        int count=-1;
+        for(int i=0;i<n;i++){
+            if(arr[i]==k){
+                count=i;
+                break;
+            }
+        }
+        if(count==-1){
+            System.out.println("Element is not present in the Array");
+        }
+        else{
+            System.out.println("Element is present at Index "+count +"\n"+"Element is present in the place "+(count+1));
+        }
+        //2D Array
+        /*int [][] arr=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.println(arr[i][j]);
+            }
+        }*/
+    }
+}
