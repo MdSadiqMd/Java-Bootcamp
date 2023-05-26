7ba15680b76f748121d6ec1ce9c3353e7fef2dc2
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-05-26 21:56:52 +0530

    Refactor --> Updated Searching Algorithms

---
+++ Java DSA BootCamp/Searching Algorithm/src/_4_Square_Root.java
@@ -0,0 +1,40 @@
+import java.util.*;
+/*
+Return the square root of the entered number , if not perfect square return the floor / Lower Bound value
+Here ,we make an array of number of elements of which we need to find square root of
+
+ */
+public class _4_Square_Root {
+    public static int BinarySearch(int n){
+      int result = 0;
+      int low=0,high=n;
+      while(low<=high){
+          int mid=low+(high-low)/2;
+          int mid2=mid*mid;
+          if(mid2==n) {
+              return mid;
+          } else if (mid2<n) {
+              result =mid;
+              low=mid+1;
+          } else {
+              high=mid-1;
+          }
+      }
+      return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+        int n=sc.nextInt();
+
+        System.out.println("Enter the Values of Array : ");
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        int result=BinarySearch(n);
+        System.out.println("The Square Root is :"+result);
+    }
+}
---
+++ Java DSA BootCamp/Sorting Algorithms/src/_1_Bubble_Sort.java
@@ -0,0 +1,15 @@
+/*
+Sorting Means to arrange the data in ascending or descending order
+In-Place --> Not Taking Extra space to sort
+Out-Place --> Taking Extra Space to Sort
+Stable Sort --> If the relative order of Array is Retained
+Non - Stable Sort --> If the relative order of Array is not Retained
+Example for Stable and Non - Stable : Array --> 1  2' 4  3  2" 5
+                                      Stable Sort --> 1  2' 2" 3 4 5
+                                      Non-Stable Sort --> 1 2" 2' 3 4 5
+ */
+public class _1_Bubble_Sort {
+    public static void main(String[] args) {
+
+    }
+}
