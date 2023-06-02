71feaecf085dfa59a92fa0674787ee1b909e809b
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-06-02 22:07:46 +0530

    Documentation --> Added time and space complexity

--- Java DSA BootCamp/Recursion/src/_1_Factorial_of_a_Number.java
+++ Java DSA BootCamp/Recursion/src/_1_Factorial_of_a_Number.java
@@ -1,5 +1,8 @@
 import java.util.*;
-//The Working of the recursive calls is due to Stack Data Structure
+//The Working of the recursive calls is due to Stack Data Structure, Every level of stack call the lower level and return the value of the top
+//so, the space complexity is O(n)
+//The recursive function makes n recursive calls, each time reducing the input value by 1 until it reaches the base case (usually when n equals 0 or 1). Therefore, the number of recursive calls is directly proportional to the input number n
+//Time Complexity is O(n)
 public class _1_Factorial_of_a_Number {
     public static int factorial(int n) {
         int result;
