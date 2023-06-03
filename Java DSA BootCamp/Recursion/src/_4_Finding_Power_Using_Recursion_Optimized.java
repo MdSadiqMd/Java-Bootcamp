ecb682233aba80d8150a6d7671a87e28eb8173d7
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-06-03 20:43:02 +0530

    Refactor --> Replaced int to BigInteger

--- Java DSA BootCamp/Recursion/src/_4_Finding_Power_Using_Recursion_Optimized.java
+++ Java DSA BootCamp/Recursion/src/_4_Finding_Power_Using_Recursion_Optimized.java
@@ -1,27 +1,30 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 // Here we divide the problem into two half
 //Even power = 2^64 = 2^32 * 2^32
 //Odd Power = 2^65 = 2 * 2^32 * 2^32
+//Here we use BigInteger to Re-Solve Future problems
 public class _4_Finding_Power_Using_Recursion_Optimized {
-    public static int power(int n,int m){
-        if(m == 0) { //Base Condition
-            return 1;
-        } else if (m%2==0) {
-            int result = power(n,m/2);
-            return result*result;
-        } else{
-            int result = power(n,(m-1)/2);
-            return n*result*result;
+    public static BigInteger power(BigInteger n, BigInteger m) {
+        if (m.equals(BigInteger.ZERO)) { // Base Condition
+            return BigInteger.ONE;
+        } else if (m.mod(BigInteger.TWO).equals(BigInteger.ZERO)) {
+            BigInteger result = power(n, m.divide(BigInteger.TWO));
+            return result.multiply(result);
+        } else {
+            BigInteger result = power(n, (m.subtract(BigInteger.ONE)).divide(BigInteger.TWO));
+            return n.multiply(result.multiply(result));
         }
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number :");
-        int n=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        BigInteger n = sc.nextBigInteger();
 
-        System.out.println("Enter the Power :");
-        int m=sc.nextInt();
+        System.out.println("Enter the Power:");
+        BigInteger m = sc.nextBigInteger();
 
-        System.out.println("The Answer is "+power(n,m));
+        System.out.println("The Answer is " + power(n, m));
     }
 }
