408397002f00d98f3170f74c94139783348f0805
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-06-03 17:55:43 +0530

    Refactor --> Removed unnessary blocks

--- Java DSA BootCamp/Recursion/src/_4_Finding_Power_Using_Recursion_Optimized.java
+++ Java DSA BootCamp/Recursion/src/_4_Finding_Power_Using_Recursion_Optimized.java
@@ -1,8 +1,13 @@
-import java.util.*;
-public class _3_Finding_Power_Using_Recursion_Brute_Force {
+import java.util.Scanner;
+// Here we divide the problem into two half
+//Even power = 2^64 = 2^32 * 2^32
+//Odd Power = 2^65 = 2 * 2^32 * 2^32
+public class _4_Finding_Power_Using_Recursion_Optimized {
     public static int power(int n,int m){
-        int result=0; //if n=0 it usually returns zero
-        if (n==1 || m==0) {
+        int result=0;
+        if (n==0){
+            result=0;
+        } else if (n==1 || m==0) {
             result=1;
         } else {
             result=n*power(n,m-1);
