ff6cbc432dc5d9282fb223d4ebf8e07d51405eb5
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-06-18 00:28:45 +0530

    Refactor --> Added Tile Problem

--- Java DSA BootCamp/Backtracking/src/_1_Tile_Problem.java
+++ Java DSA BootCamp/Backtracking/src/_1_Tile_Problem.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class _1_Tile_Problem {
+/*
+Given a board of size 2*n and tile of size 2*1 and count the total number of ways to place the tiles on the board
+possible ways to place the tile --> 1.Horizontal
+                                    2.Vertical
+Write test cases and observe the pattern
+ */
+public class _1_Tile_Problem {
+    public static int getWays(int n){
+        if(n<=3){
+            return n;
+        }
+        return getWays(n-1)+getWays(n-2);
+    }
+    public static void main(String[] args) {
+        System.out.println(getWays(5));
+    }
 }
--- Java DSA BootCamp/Backtracking/src/_2_Generate_All_Possible_SubSequences.java
+++ Java DSA BootCamp/Backtracking/src/_2_Generate_All_Possible_SubSequences.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class _2_Generate_All_Possible_SubSequences {
+public class _2_Generate_All_Possible_SubSequences {
+    public static void main(String[] args) {
+
+    }
 }
