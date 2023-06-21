0b583fa368de612d1ef654c0fcc54c0ff1e23842
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-06-21 19:57:45 +0530

    Docs --> Corrected Documentation

--- Java DSA BootCamp/Arrays/src/Monotonic_or_Not.java
+++ Java DSA BootCamp/Arrays/src/Monotonic_or_Not.java
@@ -9,17 +9,6 @@ Given an integer array nums, return true if the given array is monotonic, or fal
 **Example 1:**
 Input: nums = [1,2,2,3]
 Output: true
-
-</aside>An array is monotonic if it is either monotone increasing or monotone decreasing.
-
-An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is
-monotone decreasing if for all i <= j, nums[i] >= nums[j].
-
-Given an integer array nums, return true if the given array is monotonic, or false otherwise.
-
-Example 1:
-Input: nums = [1,2,2,3]
-Output: true
  */
 public class Monotonic_or_Not {
     public static int Checking(int[] arr){
