13008646c7a0d4b595300cb66b5dd6131cf96ef1
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-06-22 00:51:53 +0530

    Refactor --> Added Permutations with Duplicates Problem

---
+++ Java DSA BootCamp/Backtracking/src/Permutations_with_Duplicates.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Permutations_with_Duplicates {
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void permutation(int[] arr, int start, int last) {
+        if (start == last) {
+            // Base case: Print the permutation
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        } else {
+            Set<Integer> swappedElements = new HashSet<>();
+            for (int i = start; i <= last; i++) {
+                if (swappedElements.contains(arr[i])) {
+                    // Skip duplicate elements to avoid duplicate permutations
+                    continue;
+                }
+                swappedElements.add(arr[i]);
+                // Swap the current element with the element at the start index
+                swap(arr, start, i);
+                // Recursively generate permutations for the remaining elements
+                permutation(arr, start + 1, last);
+                // Restore the original order by swapping back
+                swap(arr, start, i);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2};
+        permutation(arr, 0, arr.length - 1);
+
+    }
+}
