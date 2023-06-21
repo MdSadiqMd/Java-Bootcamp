e4d6f9d5c9589c5233549508d806fadada112aed
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-06-21 21:13:37 +0530

    Debug the code

--- Java DSA BootCamp/Backtracking/src/_4_Rat_in_a_Maze_Problem.java
+++ Java DSA BootCamp/Backtracking/src/_4_Rat_in_a_Maze_Problem.java
@@ -1,66 +1,52 @@
-/*
-The rat Should move from start to last of the maze.
-Maze is represented as a 2D Array , Suppose element "0" in the maze is represented as clear path
-                                    element "-1" is represented as an obstacle
-Source -> arr[0][0]
-Destination -> arr[n-1][n-1]
-Moving Up -> i-1
-Moving Down -> i+1
-Moving Right -> j+1
-Moving Left -> j-1
- */
+import java.util.*;
+
 public class _4_Rat_in_a_Maze_Problem {
-    public static int Direction(int[][] arr, int n) {
-        if (arr[0][0] == -1) { // No entry point
-            return 0;
+    static final int MAX = 5;
+
+    static void getallpath(int[][] matrix, int n, int row,
+                           int col, List<String> ans,
+                           String cur)
+    {
+        if (row >= n || col >= n || row < 0 || col < 0
+                || matrix[row][col] == 0) {
+            return;
         }
 
-        for (int i = 0; i < n; i++) {
-            if (arr[i][0] == 0) {
-                arr[i][0] = 1;
-            } else {
-                break;
-            }
+        if (row == n - 1 && col == n - 1) {
+            ans.add(cur);
+            return;
         }
 
-        for (int j = 0; j < n; j++) {
-            if (arr[0][j] == 0) {
-                arr[0][j] = 1;
-            } else {
-                break;
-            }
-        }
+        matrix[row][col] = 0;
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < n; j++) {
-                if (arr[i][j] == -1) {
-                    continue;
-                }
+        getallpath(matrix, n, row - 1, col, ans, cur + "U");
+        getallpath(matrix, n, row, col + 1, ans, cur + "R");
+        getallpath(matrix, n, row, col - 1, ans, cur + "L");
+        getallpath(matrix, n, row + 1, col, ans, cur + "D");
 
-                // If we can reach arr[i][j] from arr[i-1][j], then increment the count
-                if (arr[i - 1][j] > 0) {
-                    arr[i][j] = arr[i][j] + arr[i - 1][j];
-                }
+        matrix[row][col] = 1;
 
-                // If we can reach arr[i][j] from arr[i][j-1], then increment the count
-                if (arr[i][j - 1] > 0) {
-                    arr[i][j] = arr[i][j] + arr[i][j - 1];
-                }
-            }
-        }
-
-        // If the final cell is blocked, return zero; otherwise, return count
-        return (Math.max(arr[n - 1][n - 1], 0));
+        return;
     }
 
-    public static void main(String[] args) {
-        int[][] arr = {
-                {0, 0, 0, 0},
-                {0, -1, 0, 0},
-                {-1, 0, 0, 0},
-                {0, 0, 0, 0},
-        };
+    static List<String> findPath(int[][] matrix, int n)
+    {
+        List<String> ans = new ArrayList<>();
+        getallpath(matrix, n, 0, 0, ans, "");
+        return ans;
+    }
 
-        System.out.println(Direction(arr, arr.length));
+    public static void main(String[] args)
+    {
+        int[][] m = { { 1, 0, 0, 0, 0 },
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 0, 1 },
+                { 0, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 1 } };
+        int n = m.length;
+        List<String> ans = findPath(m, n);
+        for (String i : ans) {
+            System.out.println(i + " ");
+        }
     }
 }
