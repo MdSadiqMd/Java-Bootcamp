49b42c379960f0f597cf601070baf71256df4148
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-07-06 23:08:08 +0530

    Refactor --> Added Hash Set Problem

---
+++ Java DSA BootCamp/HashMap/src/_1_HashSet.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class _1_HashSet {
+    public static void main(String[] args) {
+        HashSet<Integer> Data=new HashSet<>();
+        Data.add(1);
+        Data.add(2);
+        Data.add(3);
+        Data.add(4);
+        Data.add(5);
+        System.out.println(Data);
+        Data.remove(4);
+        System.out.println(Data);
+        if(Data.contains(3)){
+            System.out.println("Present");
+        }
+        else{
+            System.out.println("Not Present");
+        }
+        //Iterator --> An Iterator is created then an object is created in hashset and it points to null and will point to any random element in the hashset
+        //*** Iterator doesn't follow any order
+        Iterator<Integer> iterator=Data.iterator();
+        while (iterator.hasNext()) {
+            int value = iterator.next(); // Retrieve the value from the iterator
+            System.out.println(value); // Print the value
+        }
+    }
+}
