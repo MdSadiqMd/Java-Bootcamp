bb006316bdab0c0324311b646b22402cc5b8a3df
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-07-09 00:46:23 +0530

    Refactor --> Added Valid Parenthesis Problem

---
+++ Java DSA BootCamp/Stack/src/_2_Valid_Parentheses.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class _2_Valid_Parentheses {
+    public static Boolean BalanceParenthesis(String Input) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < Input.length(); i++) {
+            char ch = Input.charAt(i);
+            if (ch == '[' || ch == '{' || ch == '(') {
+                stack.push(ch);
+            } else if (ch == ']' || ch == '}' || ch == ')') {
+                if (stack.isEmpty())
+                    return false;
+
+                char top = stack.pop();
+                if ((ch == ']' && top != '[') || (ch == '}' && top != '{') || (ch == ')' && top != '('))
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter parentheses: ");
+        String Input = sc.nextLine();
+
+        if (BalanceParenthesis(Input)) {
+            System.out.println("Balanced");
+        } else {
+            System.out.println("Not Balanced");
+        }
+    }
+}
