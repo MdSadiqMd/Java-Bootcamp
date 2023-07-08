303e2e51075a23817d6a7bb35847bf51d6177fcb
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-07-09 00:21:39 +0530

    Refactor --> Added Non-Repeating Character Problem

--- Java DSA BootCamp/HashMap/src/_4_Non_Repeating_Character.java
+++ Java DSA BootCamp/HashMap/src/_4_Non_Repeating_Character.java
@@ -2,15 +2,31 @@ import java.util.*;
 
 public class _4_Non_Repeating_Character {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the String :");
-        String str=sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String str = sc.nextLine();
 
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<str.length();i++){
-            if(map.containsKey(str.charAt(i))){
-                map.put(str.charAt(i),map.get(str.charAt(i))+1);
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (map.containsKey(str.charAt(i))) {
+                map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+            } else {
+                map.put(str.charAt(i), 1);
             }
+        }
+
+        int result = -1;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (map.get(str.charAt(i)) == 1) {
+                result = i;
+                System.out.println("First Non-Repeating Character is: " + str.charAt(i));
+                break;
+            }
+        }
+
+        if (result == -1) {
+            System.out.println("No Character is possible");
         }
     }
 }
