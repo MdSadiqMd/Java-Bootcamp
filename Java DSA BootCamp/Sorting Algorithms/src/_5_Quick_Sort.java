83f169fb568ad19cdc9a33354d5edc051013ff6e
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-07-16 10:40:22 +0530

    Docs --> Added Code Explanation Documentation

--- Java DSA BootCamp/Sorting Algorithms/src/_5_Quick_Sort.java
+++ Java DSA BootCamp/Sorting Algorithms/src/_5_Quick_Sort.java
@@ -1,3 +1,7 @@
+/*
+Consider a Pivot element (a random element) and segregate the elements greater than pivot element to right and smaller elements to left
+Then sort the left side element by taking a pivot element and so on with the right side elements
+ */
 public class _5_Quick_Sort {
     public static void quickSort(int[] arr,int l,int h){
         if(l<h){
