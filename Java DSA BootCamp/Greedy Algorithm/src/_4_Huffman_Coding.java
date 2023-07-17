3f1d66058cc5762d9b0e54cc1b47a5b96b66e984
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-07-17 22:53:29 +0530

    Refactor --> Added Hoffman Coding Concept

---
+++ Java DSA BootCamp/Greedy Algorithm/src/_4_Huffman_Coding.java
@@ -0,0 +1,10 @@
+/*
+It is used in Sharing the data
+It's create a tree with the terms of the data's Frequency
+Represent left Node with 0 and Right Node with 1 and parent node addition of Both the Nodes
+ */
+public class _4_Huffman_Coding {
+    public static void main(String[] args) {
+
+    }
+}
