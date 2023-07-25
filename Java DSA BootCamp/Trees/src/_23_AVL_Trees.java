1e3b9bcfeb73c47c8409f628b4c80fcdebb046fc
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-07-25 19:17:21 +0530

    Refactor --> Added AVL Tree Theory

---
+++ Java DSA BootCamp/Trees/src/_23_AVL_Trees.java
@@ -0,0 +1,11 @@
+/*
+--> AVL - Adelson Velskii and Lamdis
+--> It is a Self - Balancing BST which balances when the Balance of the tree changes when Node is Added to the Tree
+--> In BST when the tree is long One-Direction Tree then the Time Complexity for searching an Element in Tree will be O(n) from O(log n)
+--> *** If (Height of left Sub - Tree) - (Height of Right Sub - Tree) = 0 (or) 1 it's a Balanced Tree
+ */
+public class _23_AVL_Trees {
+    public static void main(String[] args) {
+
+    }
+}
