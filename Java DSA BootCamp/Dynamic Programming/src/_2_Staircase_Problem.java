d34993b11a38e0ec26c7d75c80287ba0f192f1da
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-07-28 22:12:47 +0530

    Refactor --> Added Stair Case Problem Using Dynamic Programming

--- Java DSA BootCamp/Dynamic Programming/src/_2_Staircase_Problem.java
+++ Java DSA BootCamp/Dynamic Programming/src/_2_Staircase_Problem.java
@@ -1,5 +1,17 @@
 public class _2_Staircase_Problem {
+    public static int countWays(int n){
+        int prev1=1,prev2=1,current;
+        for(int i=2;i<=n;i++){
+            current=prev1+prev2;
+            prev2=prev1;
+            prev1=current;
+        }
+        return prev1;
+    }
     public static void main(String[] args) {
-
+        System.out.println("For n=1 "+countWays(1));
+        System.out.println("For n=2 "+countWays(2));
+        System.out.println("For n=3 "+countWays(3));
+        System.out.println("For n=4 "+countWays(4));
     }
 }
