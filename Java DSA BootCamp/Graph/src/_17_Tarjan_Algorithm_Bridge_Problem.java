66d436a9daa3461d43ab4e15a2801cfbe4cb8bd8
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-08-02 20:37:22 +0530

    Docs --> Added Documentation for Tarjan's Algorithm

--- Java DSA BootCamp/Graph/src/_17_Tarjan_Algorithm_Bridge_Problem.java
+++ Java DSA BootCamp/Graph/src/_17_Tarjan_Algorithm_Bridge_Problem.java
@@ -1,5 +1,22 @@
 import java.util.ArrayList;
 
+/*
+Tarjan's algorithm finds strongly connected components (SCCs) in a directed graph.
+
+Algorithm:
+1. Initialize a stack, a counter, and arrays to track traversal order, low-link values, and visited vertices.
+2. Perform a DFS traversal on the graph:
+   - Assign traversal order and low-link values to each visited vertex.
+3. During the DFS, identify SCCs:
+   - Push each visited vertex onto the stack.
+   - Update low-link values based on neighbors' values.
+   - If traversal order matches the low-link value for a vertex, pop elements from the stack to form an SCC.
+4. Continue DFS traversal until all vertices are visited.
+5. The stack contains the SCCs when the algorithm finishes.
+6. End of the algorithm.
+
+The algorithm efficiently identifies and groups strongly connected components in a directed graph.
+ */
 public class _17_Tarjan_Algorithm_Bridge_Problem {
     public static class Edge {
         int src;
---
+++ Java DSA BootCamp/Graph/src/_18_Tarjans_Algorithm_Articulation_Point.java
@@ -0,0 +1,13 @@
+public class _18_Tarjans_Algorithm_Articulation_Point {
+    public class Edge{
+        int src;
+        int dest;
+        public Edge(int s,int d){
+            this.src=s;
+            this.dest=d;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
