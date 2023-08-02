2fc11f63de23fd052879f1fda538b95bf792a10c
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-08-02 20:25:14 +0530

    Refactor --> Added Tarjan Algorithm

--- Java DSA BootCamp/Graph/src/_16_Kosaraju_Algorithm.java
+++ Java DSA BootCamp/Graph/src/_16_Kosaraju_Algorithm.java
@@ -10,42 +10,42 @@ Approach:
 Time Complexity: O(V + E), where V is the number of vertices and E is the number of edges in the graph. This is because it performs two DFS traversals on the graph.
 */
 public class _16_Kosaraju_Algorithm {
-    public static class Edge11 {
+    public static class Edge {
         int src;
         int dest;
 
-        public Edge11(int s, int d) {
+        public Edge(int s, int d) {
             this.src = s;
             this.dest = d;
         }
     }
 
-    public static void CreateGraph(ArrayList<Edge11>[] graph) {
+    public static void CreateGraph(ArrayList<Edge>[] graph) {
         for (int i = 0; i < graph.length; i++) {
-            graph[i] = new ArrayList<Edge11>();
+            graph[i] = new ArrayList<Edge>();
         }
-        graph[0].add(new Edge11(0, 2));
-        graph[0].add(new Edge11(0, 3));
-        graph[1].add(new Edge11(1, 0));
-        graph[2].add(new Edge11(2, 1));
-        graph[3].add(new Edge11(3, 4));
+        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 3));
+        graph[1].add(new Edge(1, 0));
+        graph[2].add(new Edge(2, 1));
+        graph[3].add(new Edge(3, 4));
     }
 
-    public static void DFS(ArrayList<Edge11> graph[], int curr, boolean vis[]) {
+    public static void DFS(ArrayList<Edge> graph[], int curr, boolean vis[]) {
         vis[curr] = true;
         System.out.print(curr + " ");
         for (int i = 0; i < graph[curr].size(); i++) {
-            Edge11 e = graph[curr].get(i);
+            Edge e = graph[curr].get(i);
             if (!vis[e.dest]) {
                 DFS(graph, e.dest, vis);
             }
         }
     }
 
-    public static void toposort(ArrayList<Edge11> graph[], int curr, boolean vis[], Stack<Integer> s) {
+    public static void toposort(ArrayList<Edge> graph[], int curr, boolean vis[], Stack<Integer> s) {
         vis[curr] = true;
         for (int i = 0; i < graph[curr].size(); i++) {
-            Edge11 e = graph[curr].get(i);
+            Edge e = graph[curr].get(i);
             if (!vis[e.dest]) {
                 toposort(graph, e.dest, vis, s);
             }
@@ -53,7 +53,7 @@ public class _16_Kosaraju_Algorithm {
         s.push(curr);
     }
 
-    public static void kosaraju(ArrayList<Edge11> graph[], int v, boolean vis[]) {
+    public static void kosaraju(ArrayList<Edge> graph[], int v, boolean vis[]) {
         // Sorting the Nodes
         Stack<Integer> s = new Stack<>();
         for (int i = 0; i < v; i++) {
@@ -62,15 +62,15 @@ public class _16_Kosaraju_Algorithm {
             }
         }
         // Making a Transpose Graph
-        ArrayList<Edge11> transpose[] = new ArrayList[v];
+        ArrayList<Edge> transpose[] = new ArrayList[v];
         for (int i = 0; i < transpose.length; i++) { // This loop makes the Array List null and Visited array also initialize with false to us to reuse in DFS on Transpose Graph
             vis[i] = false;
-            transpose[i] = new ArrayList<Edge11>();
+            transpose[i] = new ArrayList<Edge>();
         }
         for (int i = 0; i < v; i++) {
             for (int j = 0; j < graph[i].size(); j++) {
-                Edge11 e = graph[i].get(j);
-                transpose[e.dest].add(new Edge11(e.dest, e.src)); // This Line creates and Transpose graph
+                Edge e = graph[i].get(j);
+                transpose[e.dest].add(new Edge(e.dest, e.src)); // This Line creates and Transpose graph
             }
         }
         // Applying DFS on Transpose Graph
@@ -85,7 +85,7 @@ public class _16_Kosaraju_Algorithm {
 
     public static void main(String[] args) {
         int v = 5;
-        ArrayList<Edge11>[] graph = new ArrayList[v];
+        ArrayList<Edge>[] graph = new ArrayList[v];
         CreateGraph(graph);
         boolean[] vis = new boolean[v];
         kosaraju(graph, v, vis);
---
+++ Java DSA BootCamp/Graph/src/_17_Tarjan_Algorithm_Bridge_Problem.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class _17_Tarjan_Algorithm_Bridge_Problem {
+    public static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void CreateGraph(ArrayList<Edge>[] graph) {
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 2));
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+        graph[3].add(new Edge(3, 0));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+    }
+
+    public static void DFS(ArrayList<Edge>[] graph, int curr, boolean vis[], int dt[], int low[], int time, int parent) {
+        vis[curr] = true;
+        dt[curr] = low[curr] = ++time;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (e.dest == parent) {
+                continue;
+            } else if (!vis[e.dest]) {
+                DFS(graph, e.dest, vis, dt, low, time, curr);
+                low[curr] = Math.min(low[curr], low[e.dest]);
+                if (dt[curr] < low[e.dest]) {
+                    System.out.println("Bridge is " + curr + " --> " + e.dest);
+                }
+            } else {
+                low[curr] = Math.min(low[curr], dt[e.dest]);
+            }
+        }
+    }
+
+    public static void getBridge(ArrayList<Edge> graph[], int v, boolean vis[]) {
+        int dt[] = new int[v];
+        int low[] = new int[v];
+        int time = 0;
+        for (int i = 0; i < v; i++) {
+            if (!vis[i]) {
+                DFS(graph, i, vis, dt, low, time, -1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int v = 6;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+        for (int i = 0; i < v; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        CreateGraph(graph);
+        boolean[] vis = new boolean[v];
+        getBridge(graph, v, vis);
+    }
+}
