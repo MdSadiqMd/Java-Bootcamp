14ccc9e3b891c8ab58dff1a0327ef81dde43c382
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-08-30 19:49:33 +0530

    Docs --> Added Segment trees Documentation

---
+++ Java DSA BootCamp/Trees/src/_28_Segment_Trees.java
@@ -0,0 +1,10 @@
+/*
+Let's take an example of an array [1,2,3,4,5]. Find the sum of elements from index two to five the time complexity in worst-Case is O(n)
+To optimize this to O(logn) we use segment trees
+*** We use segment trees when we need to find sum,avg,max,min,product,... in arrays from some indexes
+ */
+public class _28_Segment_Trees {
+    public static void main(String[] args) {
+
+    }
+}
