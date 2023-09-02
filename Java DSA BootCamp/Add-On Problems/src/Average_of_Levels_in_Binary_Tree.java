4074a56d3c54e35f551fb0629465c9a625ea4547
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-02 19:44:20 +0530

    Refactor --> Added Level Order SUccessor Code

--- Java DSA BootCamp/Add-On Problems/src/Average_of_Levels_in_Binary_Tree.java
+++ Java DSA BootCamp/Add-On Problems/src/Average_of_Levels_in_Binary_Tree.java
@@ -1,29 +1,29 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Node {
+class Node1 {
     int data;
-    Node right, left;
+    Node1 right, left;
 
-    public Node(int data) {
+    public Node1(int data) {
         this.data = data;
         left = right = null;
     }
 }
 
 public class Average_of_Levels_in_Binary_Tree {
-    Node root;
+    Node1 root;
 
     Average_of_Levels_in_Binary_Tree() {
         root = null;
     }
 
-    public static void Order(Node root) {
+    public static void Order(Node1 root) {
         if (root == null) {
             return;
         }
 
-        Queue<Node> queue = new LinkedList<>();
+        Queue<Node1> queue = new LinkedList<>();
         queue.add(root);
 
         while (!queue.isEmpty()) {
@@ -31,7 +31,7 @@ public class Average_of_Levels_in_Binary_Tree {
             double levelSum = 0;
 
             for (int i = 0; i < levelSize; i++) {
-                Node currentNode = queue.poll();
+                Node1 currentNode = queue.poll();
                 levelSum += currentNode.data;
 
                 if (currentNode.left != null) {
@@ -50,13 +50,13 @@ public class Average_of_Levels_in_Binary_Tree {
 
     public static void main(String[] args) {
         Average_of_Levels_in_Binary_Tree tree = new Average_of_Levels_in_Binary_Tree();
-        tree.root = new Node(5);
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(8);
-        tree.root.left.left = new Node(1);
-        tree.root.left.right = new Node(3);
-        tree.root.right.left = new Node(7);
-        tree.root.right.right = new Node(9);
+        tree.root = new Node1(5);
+        tree.root.left = new Node1(2);
+        tree.root.right = new Node1(8);
+        tree.root.left.left = new Node1(1);
+        tree.root.left.right = new Node1(3);
+        tree.root.right.left = new Node1(7);
+        tree.root.right.right = new Node1(9);
 
         Order(tree.root);
     }
---
+++ Java DSA BootCamp/Add-On Problems/src/Level_Order_Successor_of_a_Node.java
@@ -0,0 +1,80 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class Level_Order_Successor_of_a_Node {
+    Node root;
+
+    Level_Order_Successor_of_a_Node() {
+        root = null;
+    }
+
+    public Node findSuccessor(Node root, int target) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        boolean foundTarget = false;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            foundTarget = false;
+
+            for (int i = 0; i < levelSize; i++) {
+                Node current = queue.poll();
+
+                // If the target node was found in the previous level, the current node is its successor.
+                if (foundTarget) {
+                    return current;
+                }
+
+                if (current.data == target) {
+                    foundTarget = true; // Mark that we found the target node.
+                }
+
+                // Add the children to the queue.
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+        }
+
+        // If the target node was not found or is the last node in the tree, there's no successor.
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Level_Order_Successor_of_a_Node tree = new Level_Order_Successor_of_a_Node();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.right.left = new Node(5);
+        tree.root.right.right = new Node(6);
+        tree.root.right.left.left = new Node(7);
+        tree.root.right.left.right = new Node(8);
+
+        int target = 6;
+        Node successor = tree.findSuccessor(tree.root, target);
+
+        if (successor != null) {
+            System.out.println("The level order successor of " + target + " is " + successor.data);
+        } else {
+            System.out.println(target + " does not have a level order successor.");
+        }
+    }
+}
