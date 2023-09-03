063194c73994a40023b5e4744fbc19f63f6d4bfe
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-03 21:44:18 +0530

    Refactor --> Added Contains Duplicate Problem

--- Java DSA BootCamp/Add-On Problems/src/Contains_Duplicate.java
+++ Java DSA BootCamp/Add-On Problems/src/Contains_Duplicate.java
@@ -1,5 +1,18 @@
 public class Contains_Duplicate {
+    public static boolean Check(int[] nums){
+        if(nums!=null){
+            for(int i=0;i<nums.length-1;i++){
+                for(int j=i+1;j<nums.length;j++){
+                    if(nums[i]==nums[j]){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
-
+        int[] nums={1,2,3,1};
+        System.out.println(Check(nums));
     }
 }
