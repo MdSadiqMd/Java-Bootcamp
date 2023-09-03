1a32d89a3fea9a440b7d994edcc5c068fd838a6e
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-03 21:06:05 +0530

    Refactor --> Added Longest Common Prefix Problem

--- Java DSA BootCamp/Add-On Problems/src/Longest_Common_Prefix.java
+++ Java DSA BootCamp/Add-On Problems/src/Longest_Common_Prefix.java
@@ -1,5 +1,21 @@
 public class Longest_Common_Prefix {
-    public static void main(String[] args) {
+    public static StringBuilder commonPrefix(String[] s) {
+        StringBuilder ans = new StringBuilder();
+        if(s!=null){
+            for (int i = 0; i <s.length-1; i++) {
+                char currentChar = s[0].charAt(i);
+                for (int j = 1; j < s.length; j++) {
+                    if (s[j].charAt(i) == currentChar) {
+                        ans.append(currentChar);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
 
+    public static void main(String[] args) {
+        String[] s = { "flower", "flow", "sight" };
+        System.out.println(commonPrefix(s));
     }
 }
