1a0b6f9b19f47b55422900ecea08a9998d6ac2ff
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-04 18:01:12 +0530

    Refactor --> Added Insertion Code

--- Java DSA BootCamp/Add-On Problems/src/Generate_Parenthesis.java
+++ Java DSA BootCamp/Add-On Problems/src/Generate_Parenthesis.java
@@ -1,5 +1,17 @@
-public class Generate_Parenthesis {
-    public static void main(String[] args) {
+import java.util.Stack;
 
+public class Generate_Parenthesis {
+    public static void Generate(int n){
+        int[] ans=new int[3];
+        Stack<Object> stack=new Stack<>();
+        for(int i=0;i<n;i++){
+            stack.push("(");
+            stack.push(")");
+        }
+
+    }
+    public static void main(String[] args) {
+        int n=3;
+        Generate(n);
     }
 }
