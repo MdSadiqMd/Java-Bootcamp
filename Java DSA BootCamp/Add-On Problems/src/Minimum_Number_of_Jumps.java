91866e7595dbab823375bab31b60db1925ee4e8b
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-06 20:57:08 +0530

    Refactor --> Added Minimum number of jumps Problem

--- Java DSA BootCamp/Add-On Problems/src/Minimum_Number_of_Jumps.java
+++ Java DSA BootCamp/Add-On Problems/src/Minimum_Number_of_Jumps.java
@@ -3,7 +3,18 @@ Given an array of integers where each element represents the max number of steps
 If an element is 0, then you cannot move through that element.
  */
 public class Minimum_Number_of_Jumps {
+    public static int jumps(int[] arr){
+        int ans=0;
+        for(int i=0;i<arr.length-1;i++){
+            int k=arr[0];
+            for(int j=1;j<arr[0]-1;j++){
+                ans=Math.max(arr[j]+j,arr[j+1]+j+1);
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
-
+        int[] arr={1,3,5,8,9,2,6,8,2,9,10};
+        System.out.println(jumps(arr));
     }
 }
