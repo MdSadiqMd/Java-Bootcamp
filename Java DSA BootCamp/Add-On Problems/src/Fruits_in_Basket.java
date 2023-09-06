6fa99400e0045d06845d6da848ebd3e0acf4ac76
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-06 20:18:33 +0530

    Refactor --> Added Fruits in basket Problem

--- Java DSA BootCamp/Add-On Problems/src/Fruits_in_Basket.java
+++ Java DSA BootCamp/Add-On Problems/src/Fruits_in_Basket.java
@@ -1,30 +1,44 @@
 import java.util.HashMap;
 import java.util.Map;
-
+/*
+This Problem will change with some words itself like
+Inplace of Array it may be string to find the substring with distinct words
+or a value k is given and asked to find the kth number of substrings
+ */
 public class Fruits_in_Basket {
-    public static int totalFruit(int[] arr){
-        int left=0;
-        int right=0;
-        int total=0;
-        Map<Integer,Integer> basket=new HashMap<>();
-        for(right=0;right<arr.length;right++){
-            int currentCount=basket.getOrDefault(arr[right],0);
-            basket.put(arr[right],currentCount+1);
-            while(basket.size()>2){
-                int fruitCount=basket.get(arr[left]);
-                if(fruitCount==1){
-                    basket.remove(arr[left]);
-                } else {
-                    basket.put(arr[left],fruitCount-1);
+    public static int totalFruit(int[] arr) {
+        int left = 0;
+        int right = 0;
+        int total = 0;
+        Map<Integer, Integer> basket = new HashMap<>();
+        for (right = 0; right < arr.length; right++) {
+            // Check if the key exists in the map
+            if (basket.containsKey(arr[right])) {
+                int currentCount = basket.get(arr[right]);
+                basket.put(arr[right], currentCount + 1);
+            } else {
+                basket.put(arr[right], 1); // Initialize count to 1 if the key is not present
+            }
+
+            while (basket.size() > 2) {
+                // Check if the key exists in the map before accessing it
+                if (basket.containsKey(arr[left])) {
+                    int fruitCount = basket.get(arr[left]);
+                    if (fruitCount == 1) {
+                        basket.remove(arr[left]);
+                    } else {
+                        basket.put(arr[left], fruitCount - 1);
+                    }
                     left++;
                 }
-                total=Math.max(total,right-left+1);
+                total = Math.max(total, right - left + 1);
             }
         }
         return total;
     }
+
     public static void main(String[] args) {
-        int[] arr={1,2,1,1,3,4,2,2,2,2,4};
+        int[] arr = {1, 2, 1, 1, 3, 4, 2, 2, 2, 2, 4};
         System.out.println(totalFruit(arr));
     }
 }
