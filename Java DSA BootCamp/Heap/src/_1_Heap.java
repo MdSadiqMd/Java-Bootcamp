54eeb97cde73fe60c0e75ed4369de73d166a8db9
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-26 14:49:17 +0530

    Refactor --> Added Heap Data Structure

---
+++ Java DSA BootCamp/Heap/src/_1_Heap.java
@@ -0,0 +1,9 @@
+/*
+The point of using heap is to search an Element in an Array in O(logn) time complexity
+Here we represent the Array in the Form of Complete Binary Tree calling it as Heap
+ */
+public class _1_Heap {
+    public static void main(String[] args) {
+
+    }
+}
