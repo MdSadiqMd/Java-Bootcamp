c0137940b4aece82c9b9d3f43f1e6a833ec9c04a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-18 20:47:54 +0530

    Refactor --> Added Count Sort Algorithm

--- Java DSA BootCamp/Sorting Algorithms/src/_8_Count_Sort.java
+++ Java DSA BootCamp/Sorting Algorithms/src/_8_Count_Sort.java
@@ -6,7 +6,37 @@ Usually preferable for only small numbers Sorting
 3. Return the Elements of the Array with respect to frequencies
  */
 public class _8_Count_Sort {
+    public static void countSort(int[] arr){
+       if(arr.length <=1){
+           return;
+       }
+       // Finding Largest Element
+       int max=arr[0];
+       for(int num:arr){
+           if(num>max){
+               max=num;
+           }
+       }
+       int[] countArray=new int[max+1];
+       // Creating Frequency Array
+        for(int num:arr){
+            countArray[num]++;
+        }
+        // Inserting in sorted Order with respect to frequency Array
+        int index=0;
+        for(int i=0;i<max;i++){
+            while(countArray[i]>0){
+                arr[index]=i;
+                index++;
+                countArray[i]--;
+            }
+        }
+    }
     public static void main(String[] args) {
-
+        int[] arr={3,4,1,3,2,5,2,8};
+        countSort(arr);
+        for(int i=0;i<arr.length-1;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
