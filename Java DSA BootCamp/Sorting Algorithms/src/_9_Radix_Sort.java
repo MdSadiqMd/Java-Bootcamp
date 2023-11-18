341982e5bc68f4a61e0d4f3edb8cd713f19e61e6
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-18 21:30:13 +0530

    Refactor --> Added Radix Sort Algorithm

--- Java DSA BootCamp/Sorting Algorithms/src/_9_Radix_Sort.java
+++ Java DSA BootCamp/Sorting Algorithms/src/_9_Radix_Sort.java
@@ -1,10 +1,36 @@
-/*
-Radix Sort is basically used to sort large Numbers
-1. Identify the Largest Number in the Array
-2. Sort the array digits by using count Sort upto "n" number of times (where n is the number of sights of the largest Integer)
- */
-public class _9_Radix_Sort {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class _9_Radix_Sort {
+    public static void radixSort(int[] arr) {
+        int max = Arrays.stream(arr).max().getAsInt();
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countSort(arr, exp);
+        }
+    }
+    private static void countSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+        Arrays.fill(count, 0);
+        // Count occurrences
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+        // Update count array
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+        // Build the output array
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+        System.arraycopy(output, 0, arr, 0, n);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {29, 83, 471, 36, 91, 8};
+        radixSort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
     }
 }
