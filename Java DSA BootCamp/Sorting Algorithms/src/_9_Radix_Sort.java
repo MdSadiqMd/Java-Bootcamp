6b2b2fafa2302a81ce4ebcfe17f81a096c0f5696
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-18 20:56:45 +0530

    Docs --> Added Radix Sort Documentation

--- Java DSA BootCamp/Sorting Algorithms/src/_9_Radix_Sort.java
+++ Java DSA BootCamp/Sorting Algorithms/src/_9_Radix_Sort.java
@@ -1,3 +1,8 @@
+/*
+Radix Sort is basically used to sort large Numbers
+1. Identify the Largest Number in the Array
+2. Sort the array digits by using count Sort upto "n" number of times (where n is the number of sights of the largest Integer)
+ */
 public class _9_Radix_Sort {
     public static void main(String[] args) {
 
