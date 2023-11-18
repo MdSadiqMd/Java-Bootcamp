c36e793ca0436fc29f5ae8b61b2ba369b8e74702
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-18 20:28:32 +0530

    Docs --> Added Count Sort ALgorithm Documentation

--- Java DSA BootCamp/Sorting Algorithms/src/_8_Count_Sort.java
+++ Java DSA BootCamp/Sorting Algorithms/src/_8_Count_Sort.java
@@ -1,3 +1,10 @@
+/*
+Count Sort is a non-Comparison based Sorting Algorithm\
+Usually preferable for only small numbers Sorting
+1. Find the largest number in the array
+2. Create a new Array of size of the largest Element+1 and enter the frequencies of the Input Array elements in the new Array Created
+3. Return the Elements of the Array with respect to frequencies
+ */
 public class _8_Count_Sort {
     public static void main(String[] args) {
 
