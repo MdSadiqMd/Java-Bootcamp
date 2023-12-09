3de01862080b3dc497d870e02526f84a34de317b
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-12-09 10:54:57 +0530

    Refactor --> Added Print all balance Pranthesis Problem

--- Java DSA BootCamp/Add-On Problems/src/PrintAllBalanceParenthesis.java
+++ Java DSA BootCamp/Add-On Problems/src/PrintAllBalanceParenthesis.java
@@ -1,4 +1,17 @@
 public class PrintAllBalanceParenthesis {
+    public void balanceParams(char[] arr,int n,int i,int open,int close){
+        if(i==arr.length){
+            System.out.println(arr[i]);
+        }
+        if(open<close){
+            arr[i]='(';
+            balanceParams(arr,n,i+1,open+1,close);
+        }
+        if(close<open){
+            arr[i]=')';
+            balanceParams(arr,n,i+1,close,open+1);
+        }
+    }
     public static void main(String[] args) {
 
     }
