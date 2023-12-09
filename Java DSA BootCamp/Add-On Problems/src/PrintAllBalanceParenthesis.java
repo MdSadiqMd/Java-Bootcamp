b44ebfcbf89c7bf0b1a2c171228559559220cde0
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-12-09 11:02:42 +0530

    Refactor --> Added Print all balance Pranthesis Problem

--- Java DSA BootCamp/Add-On Problems/src/PrintAllBalanceParenthesis.java
+++ Java DSA BootCamp/Add-On Problems/src/PrintAllBalanceParenthesis.java
@@ -1,18 +1,22 @@
+// Approach --> Recursive Tree which go along splitting with respect to conditions and backtrack with answers --> Watch Tap academy Video
 public class PrintAllBalanceParenthesis {
-    public void balanceParams(char[] arr,int n,int i,int open,int close){
-        if(i==arr.length){
-            System.out.println(arr[i]);
+    public static void balanceParams(char[] arr, int n, int i, int open, int close) {
+        if (i == arr.length) {
+            System.out.println(new String(arr));
+            return;
         }
-        if(open<close){
-            arr[i]='(';
-            balanceParams(arr,n,i+1,open+1,close);
+        if (open < n) {
+            arr[i] = '(';
+            balanceParams(arr, n, i + 1, open + 1, close);
         }
-        if(close<open){
-            arr[i]=')';
-            balanceParams(arr,n,i+1,close,open+1);
+        if (close < open) {
+            arr[i] = ')';
+            balanceParams(arr, n, i + 1, open, close + 1);
         }
     }
     public static void main(String[] args) {
-
+        int n = 5;
+        char[] arr = new char[n * 2];
+        balanceParams(arr, n, 0, 0, 0);
     }
 }
