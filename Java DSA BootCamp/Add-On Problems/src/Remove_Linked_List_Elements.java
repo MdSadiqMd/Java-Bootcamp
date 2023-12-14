cb43f1da8d238a2bb85c14b1cefeb797f99785e4
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-12-14 21:46:55 +0530

    Refactor --> Added Remove Linked List Elemnts Code

--- Java DSA BootCamp/Add-On Problems/src/Remove_Linked_List_Elements.java
+++ Java DSA BootCamp/Add-On Problems/src/Remove_Linked_List_Elements.java
@@ -1,5 +1,19 @@
 public class Remove_Linked_List_Elements {
+    static class Node{
+        Node head;
+        Node next;
+        int data;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
     public static void main(String[] args) {
-
+        int n=6;
+        Node list=new Node(1);
+        list.next=new Node(2);
+        list.next.next=new Node(3);
+        list.next.next.next=new Node(4);
+        list.next.next.next.next=new Node(3);
     }
 }
