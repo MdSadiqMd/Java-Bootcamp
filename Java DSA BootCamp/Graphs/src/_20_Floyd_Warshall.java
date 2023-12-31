1c94e11e6c4ff0c9d6c49ab87961b47940ae4aef
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-12-31 19:57:37 +0530

    Refactor --> Added Floyd Warshall ALgorithm

--- Java DSA BootCamp/Graphs/src/_20_Floyd_Warshall.java
+++ Java DSA BootCamp/Graphs/src/_20_Floyd_Warshall.java
@@ -1,4 +1,9 @@
 import java.util.ArrayList;
+// This Floyd Warshall Algorithm computes the distance from one node to all other nodes and so on from other Node to all other Nodes
+// This is usually is used to find the Minimum distance from one Node to another Node
+// Here we use Adjacency Matrix Method other than Adjacency List
+// *** The Time complexity is O(n^3) which is *not Efficient, but it works for negative weights
+// *** IF positive weights use Bellman Ford if negative weights use this algorithm
 
 public class _20_Floyd_Warshall {
     static class Edge{
@@ -9,6 +14,48 @@ public class _20_Floyd_Warshall {
             this.src=s;
             this.dest=d;
             this.wt=w;
+        }
+    }
+
+    public static void floydWarshall(ArrayList<Edge>[] graph){
+        int V=graph.length;
+        int[][] dist=new int[V][V];
+
+        // Initialise Matrix
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(i==j){
+                    dist[i][j]=0;
+                } else {
+                    dist[i][j]= Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        // Set Initial distances from graph
+        for (ArrayList<Edge> edges : graph) {
+            for (Edge edge : edges) {
+                dist[edge.src][edge.dest] = edge.wt;
+            }
+        }
+
+        // Floyd Warshall Algorithm
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                for(int k=0;k<V;k++){
+                    if(dist[i][k]!= Integer.MAX_VALUE && dist[k][j]!= Integer.MAX_VALUE && dist[i][k]+dist[k][j]<dist[i][j]){
+                        dist[i][j]=dist[i][k]+dist[k][j];
+                    }
+                }
+            }
+        }
+
+        // Printing Result
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                System.out.print(dist[i][j]+" --> ");
+            }
+            System.out.println();
         }
     }
 
@@ -31,5 +78,6 @@ public class _20_Floyd_Warshall {
         int v=4;
         ArrayList<Edge>[] graph =new ArrayList[v];
         CreateGraph(graph);
+        floydWarshall(graph);
     }
 }
