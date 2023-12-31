617faf411565e8bc311c9caf9e99d6508e7e7a5d
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-12-31 19:35:01 +0530

    Refactor --> Added Floyd Warshall ALgorithm

---
+++ Java DSA BootCamp/Graphs/src/_20_Floyd_Warshall.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class _20_Floyd_Warshall {
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+        public Edge(int s,int d,int w){
+            this.src=s;
+            this.dest=d;
+            this.wt=w;
+        }
+    }
+
+    public static void CreateGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1, 10));
+        graph[0].add(new Edge(0, 2, 15));
+        graph[0].add(new Edge(0, 3, 30));
+        graph[1].add(new Edge(1, 0, 10));
+        graph[1].add(new Edge(1, 3, 40));
+        graph[2].add(new Edge(2, 0, 15));
+        graph[2].add(new Edge(2, 3, 50));
+        graph[3].add(new Edge(3, 1, 40));
+        graph[3].add(new Edge(3, 2, 50));
+    }
+
+    public static void main(String[] args) {
+        int v=4;
+        ArrayList<Edge>[] graph =new ArrayList[v];
+        CreateGraph(graph);
+    }
+}
