b1320a9093090b86748061a9cb69a330c01a83ee
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-01-05 20:09:51 +0530

    Refactor --> Added ReOrder List

--- Java DSA BootCamp/Add-On Problems/src/ReOrder_List.java
+++ Java DSA BootCamp/Add-On Problems/src/ReOrder_List.java
@@ -8,43 +8,44 @@ public class ReOrder_List {
         }
     }
     private Node current;
+
     public void reorderList(Node head) {
         if (head == null || head.next == null) {
             return;
         }
         current = head;
-        reorderListHelper(head);
+        reorderListHelper(head.next);
     }
     private void reorderListHelper(Node node) {
-        // Base case: If the current node is null, stop the recursion
-        if (node == null) {
+        // Base case: If the current node is null or the next node is null, stop the recursion
+        if (node == null || node.next == null) {
             return;
         }
-        // Recursive call: Move to the next node in the list
-        reorderListHelper(node.next);
-        // Reordering logic during the backtracking phase
-        if (current != node && current.next != null) {
-            Node temp = current.next;
-            current.next = node;
-            node.next = temp;
-            current = temp;
-        } else {
-            current.next = null; // Set next of the last node to null
-        }
-    }
 
+        // Recursive call: Move to the next node in the list
+        reorderListHelper(node.next.next);
+
+        // Reordering logic during the backtracking phase
+        Node temp = current.next;
+        current.next = node.next;
+        node.next = null; // Set next of the current node to null to avoid infinite loop
+        current.next.next = temp;
+        current = temp;
+    }
     public static void main(String[] args) {
         ReOrder_List reorderList = new ReOrder_List();
         Node list = new Node(1);
         list.next = new Node(2);
         list.next.next = new Node(3);
         list.next.next.next = new Node(4);
+        // Create a separate pointer to the head of the list
+        Node originalList = list;
         // Call the reorderList method to initiate the reordering
         reorderList.reorderList(list);
-        // Print the modified list
-        while (list != null) {
-            System.out.print(list.data + " -> ");
-            list = list.next;
+
+        while (originalList != null) {
+            System.out.print(originalList.data + " -> ");
+            originalList = originalList.next;
         }
         System.out.println("null");
     }
---
+++ Java DSA BootCamp/Add-On Problems/src/Sort_a_Linked_List.java
@@ -0,0 +1,16 @@
+public class Sort_a_Linked_List {
+    static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static void sort(Node head){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
