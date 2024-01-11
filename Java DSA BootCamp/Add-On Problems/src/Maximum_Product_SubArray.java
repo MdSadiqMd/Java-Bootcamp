8341cd9d4e27e4fe60b5f7085a7e9be45cb0b286
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-01-11 21:43:03 +0530

    Refactor --> Addded Maximum product sub array problem

---
+++ Java DSA BootCamp/Add-On Problems/src/Maximum_Product_SubArray.java
@@ -0,0 +1,29 @@
+/*
+
+ */
+public class Maximum_Product_SubArray {
+    public static int sum(int[] arr){
+        int leftTraversalSum=1;
+        int rightTraversalSum=1;
+        int ans=Integer.MIN_VALUE;
+        if(arr.length==1){
+            return arr[0];
+        }
+        for(int i=0;i<arr.length-1;i++){
+            if(leftTraversalSum==0){
+                leftTraversalSum=1;
+            }
+            if(rightTraversalSum==0){
+                rightTraversalSum=1;
+            }
+            leftTraversalSum=leftTraversalSum*arr[i];
+            rightTraversalSum=rightTraversalSum*arr[i];
+            ans=Math.max(ans,Math.max(leftTraversalSum,rightTraversalSum));
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr={2,3,-2,4};
+        System.out.println(sum(arr));
+    }
+}
