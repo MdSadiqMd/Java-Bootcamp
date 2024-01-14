aef90b9a9379cf5ad9e81108f63556675cac012f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-01-14 10:03:40 +0530

    Refactor --> Added Fibonnachi Series Using DP

--- Java DSA BootCamp/Dynamic Programming/src/_1_Fibonacci_Series.java
+++ Java DSA BootCamp/Dynamic Programming/src/_1_Fibonacci_Series.java
@@ -1,41 +1,39 @@
+import java.util.Arrays;
+
 public class _1_Fibonacci_Series {
-    // Top - Down Approach
-    public static int fibonacci(int n){
-        int a=0,b=1,c;
+    // Top - Down Approach --> Memoization --> Time and Space Complexity --> O(N)
+    public static int fibonacci(int n,int[] dp){
         if(n==0 || n==1){
             return n;
-        }
-        else{
-            for(int i=2;i<=n;i++){
-                c=a+b;
-                a=b;
-                b=c;
-            }
-            return b;
-        }
-    }
-
-    // Bottom - Up Approach
-    public static int fibArray(int n,int[] fibSeries){
-        if(n==0 || n==1){
-            return n;
-        } else if (fibSeries[n]!=-1) {
-            return fibSeries[n];
+        } else if (dp[n]!=-1) {
+            return dp[n];
         } else {
-            return fibArray(n-1,fibSeries)+fibArray(n-2,fibSeries);
+            return fibonacci(n-1,dp)+fibonacci(n-2,dp);
         }
     }
-    public static int fibonacci_1(int n){
-        int[] fibSeries=new int[n+1];
-        for(int i=0;i<=n;i++){
-            fibSeries[i]=-1; //Keeping the ALl Elements in Array to -1
-        }
-        return fibArray(n,fibSeries);
-    }
-
     public static void main(String[] args) {
         int n=7;
-        System.out.println("Top - Down Approach "+fibonacci(n));
-        System.out.println("Bottom - Up Approach "+fibonacci_1(n));
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        System.out.println("Top - Down Approach --> Memoization "+fibonacci(n,dp));
+
+        // Bottom - Up Approach --> Tabulation --> Here we already hard-code the base case condition and reduce the base case condition computation time --> Time and Space Complexity --> O(N)
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        System.out.println("Bottom - Up Approach --> Tabulation "+dp[n]);
+
+        // Space Optimization --> If we see we only need the value of prev two and for the next the prev two will be updated
+        int prev2=0;
+        int prev1=1;
+        for(int i=2;i<=n;i++){
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        System.out.println("Space Optimization Approach "+prev1);
+
     }
 }
