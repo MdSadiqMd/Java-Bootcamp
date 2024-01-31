f944eec533f78a0a73520c782ce89e074492ed59
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-01-31 19:34:39 +0530

    Refactor --> Added SubsetSum Equal to Target K Problem

--- Java DSA BootCamp/Dynamic Programming/src/_19_Subset_Sum_Equal_To_Target.java
+++ Java DSA BootCamp/Dynamic Programming/src/_19_Subset_Sum_Equal_To_Target.java
@@ -1,10 +1,17 @@
 import java.util.Arrays;
 
 public class _19_Subset_Sum_Equal_To_Target {
+    // SubsetSum Equal to Target --> https://takeuforward.org/data-structure/subset-sum-equal-to-target-dp-14/
+
+    // Top - Down Approach --> Memoization
     public static boolean sum(int index,int target,int[] arr,int[][] dp){
         if(index==0) return arr[0]==target;
         if(target==0) return true;
-        if(dp[index][target]!=-1) return dp[index][target]==0?false:true;
+        if(dp[index][target]!=-1) return dp[index][target] != 0;
+        boolean notTake=sum(index-1,target,arr,dp);
+        boolean take=false;
+        if(arr[index]<=target) take=sum(index-1,target-arr[index],arr,dp);
+        return take || notTake;
     }
     public static void main(String[] args) {
         int[] arr={1,2,3,4};
@@ -12,5 +19,6 @@ public class _19_Subset_Sum_Equal_To_Target {
         int k=4;
         int[][] dp=new int[n][k+1];
         for(int[] row:dp) Arrays.fill(row,-1);
+        System.out.println(sum(n-1,k,arr,dp));
     }
 }
