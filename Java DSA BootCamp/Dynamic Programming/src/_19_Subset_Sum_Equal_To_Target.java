6562be38ca2dd100cd30aa7ec8475b9a7642bf44
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-01-31 19:49:37 +0530

    Refactor --> Added SubsetSum Equal to Target K Problem

--- Java DSA BootCamp/Dynamic Programming/src/_19_Subset_Sum_Equal_To_Target.java
+++ Java DSA BootCamp/Dynamic Programming/src/_19_Subset_Sum_Equal_To_Target.java
@@ -13,6 +13,22 @@ public class _19_Subset_Sum_Equal_To_Target {
         if(arr[index]<=target) take=sum(index-1,target-arr[index],arr,dp);
         return take || notTake;
     }
+    // Bottom - Up Approach --> Tabulation
+    public static boolean sumTab(int n, int k, int[] arr) {
+        boolean[][] dp = new boolean[n][k + 1];
+        for (int i = 0; i < n; i++) dp[i][0] = true;
+        if (arr[0] <= k) dp[0][arr[0]] = true;
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target <= k; target++) {
+                boolean notTake = dp[ind - 1][target];
+                boolean take = false;
+                if (arr[ind] <= target) take = dp[ind - 1][target - arr[ind]];
+                dp[ind][target] = notTake || take;
+            }
+        }
+        return dp[n - 1][k];
+    }
+
     public static void main(String[] args) {
         int[] arr={1,2,3,4};
         int n=arr.length;
@@ -20,5 +36,6 @@ public class _19_Subset_Sum_Equal_To_Target {
         int[][] dp=new int[n][k+1];
         for(int[] row:dp) Arrays.fill(row,-1);
         System.out.println(sum(n-1,k,arr,dp));
+        System.out.println(sumTab(n-1,k,arr));
     }
 }
