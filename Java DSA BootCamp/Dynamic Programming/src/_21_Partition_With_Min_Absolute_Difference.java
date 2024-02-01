de045bf99dea73a104d411619f3c1bf8beee27c3
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-01 22:14:01 +0530

    Refactor --> Added Partition with Min Difference Problem

---
+++ Java DSA BootCamp/Dynamic Programming/src/_21_Partition_With_Min_Absolute_Difference.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class _21_Partition_With_Min_Absolute_Difference {
+    // Partition set into two subsets with Min Absolute Difference --> https://takeuforward.org/data-structure/partition-set-into-2-subsets-with-min-absolute-sum-diff-dp-16/
+
+    // Top - Down Approach --> Memoization
+    public static int partition(int n,ArrayList<Integer> arr){
+
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        int n = arr.size();
+        System.out.println(partition(n,arr));
+    }
+}
