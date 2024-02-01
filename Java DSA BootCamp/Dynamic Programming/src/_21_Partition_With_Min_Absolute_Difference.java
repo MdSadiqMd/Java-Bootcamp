daacdd769921ef38f8b43bc2eb0233d81d012553
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-01 22:48:43 +0530

    Refactor --> Added Partition with Min Difference Problem

--- Java DSA BootCamp/Dynamic Programming/src/_21_Partition_With_Min_Absolute_Difference.java
+++ Java DSA BootCamp/Dynamic Programming/src/_21_Partition_With_Min_Absolute_Difference.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class _21_Partition_With_Min_Absolute_Difference {
     // Partition set into two subsets with Min Absolute Difference --> https://takeuforward.org/data-structure/partition-set-into-2-subsets-with-min-absolute-sum-diff-dp-16/
 
-    // Top - Down Approach --> Memoization
+    // Top - Down Approach --> Memoization, Tabulation
     public static int partition(int n, ArrayList<Integer> arr){
         int sum = 0;
         for(int i = 0; i < n; i++) sum += arr.get(i);
@@ -22,9 +22,33 @@ public class _21_Partition_With_Min_Absolute_Difference {
         for(int i = 0; i <= sum; i++) if(dp[n - 1][i]) min = Math.min(min, Math.abs(i - (sum - i)));
         return min;
     }
+    // Space Optimization
+    public static int partitionSpace(int n, ArrayList<Integer> arr) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) sum += arr.get(i);
+        boolean[] prev = new boolean[sum + 1];
+        prev[0] = true;
+        if (arr.get(0) <= sum) prev[arr.get(0)] = true;
+        for (int index = 1; index < n; index++) {
+            boolean[] curr = new boolean[sum + 1];
+            curr[0] = true;
+            for (int target = 1; target <= sum; target++) {
+                boolean notTaken = prev[target];
+                boolean taken = false;
+                if (arr.get(index) <= target) taken = prev[target - arr.get(index)];
+                curr[target] = taken || notTaken;
+            }
+            prev = curr;
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i <= sum; i++) if (prev[i]) min = Math.min(min, Math.abs(i - (sum - i)));
+        return min;
+    }
+
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
         int n = arr.size();
         System.out.println(partition(n,arr));
+        System.out.println(partitionSpace(n,arr));
     }
 }
