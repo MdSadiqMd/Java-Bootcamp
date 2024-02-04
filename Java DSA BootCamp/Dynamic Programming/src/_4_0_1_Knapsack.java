2c906caa277145b586d72921ee4f3bf577d475c1
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-04 20:10:14 +0530

    Refactor --> Added Memoization Approach

--- Java DSA BootCamp/Dynamic Programming/src/_4_0_1_Knapsack.java
+++ Java DSA BootCamp/Dynamic Programming/src/_4_0_1_Knapsack.java
@@ -1,7 +1,25 @@
 import java.util.Arrays;
-public class _4_0_1_Knapsack {
-    // 0/1 Knapsack -->
-    public static void main(String[] args) {
 
+public class _4_0_1_Knapsack {
+    // 0/1 Knapsack --> https://takeuforward.org/data-structure/0-1-knapsack-dp-19/
+
+    // Top - Down Approach --> Memoization
+    public static int knapsack(int capacity,int index,int[] wt,int[] val,int[][] dp){
+        if(index==0){
+            return  (wt[0]<=capacity) ? val[0] : 0;
+        }
+        if(dp[index][capacity]!=-1) return dp[index][capacity];
+        int notTake = knapsack(capacity, index - 1, wt, val, dp);
+        int take = (wt[index] <= capacity) ? val[index] + knapsack(capacity - wt[index], index - 1, wt, val, dp) : 0;
+        return dp[index][capacity]=Math.max(notTake,take);
+    }
+    public static void main(String[] args) {
+        int[] wt = {1, 2, 4, 5};
+        int[] val = {5, 4, 8, 6};
+        int capacity = 5;
+        int n = wt.length;
+        int[][] dp=new int[n][capacity+1];
+        for(int[] row:dp) Arrays.fill(row,-1);
+        System.out.println(knapsack(capacity,n-1,wt,val,dp));
     }
 }
