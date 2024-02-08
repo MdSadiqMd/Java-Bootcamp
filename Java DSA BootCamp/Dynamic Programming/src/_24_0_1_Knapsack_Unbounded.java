0ab02fcb70a327419dbd84880fe774c612c5f04f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-08 20:53:34 +0530

    Refactor --> Added Unbounded KnapSack Problem

--- Java DSA BootCamp/Dynamic Programming/src/_24_0_1_Knapsack_Unbounded.java
+++ Java DSA BootCamp/Dynamic Programming/src/_24_0_1_Knapsack_Unbounded.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.concurrent.LinkedBlockingDeque;
 
 public class _24_0_1_Knapsack_Unbounded {
     public static int knapsack(int index,int capacity,int[] wt,int[] val,int[][] dp){
@@ -9,6 +8,18 @@ public class _24_0_1_Knapsack_Unbounded {
         int take= (wt[index]<=capacity) ? val[index]+knapsack(index,capacity-wt[index],wt,val,dp) : 0;
         return dp[index][capacity]=Math.max(notTake,take);
     }
+    public static int knapsackTab(int index,int capacity,int[] wt,int[] val,int[][] dp){
+        if(index==0) return (capacity/wt[0])*val[0];
+        if(dp[index][capacity]!=-1) return dp[index][capacity];
+        for(int ind=1;ind<index;ind++){
+            for(int T=0;T<=capacity;T++){
+                int notTake=dp[ind-1][T];
+                int take= (wt[ind]<=T) ? val[ind]+dp[ind][T-wt[ind]] : 0;
+                dp[index][capacity]=Math.max(notTake,take);
+            }
+        }
+        return dp[index-1][capacity];
+    }
     public static void main(String[] args) {
         int[] wt = {2,4,6};
         int[] val = {5,11,13};
@@ -17,5 +28,6 @@ public class _24_0_1_Knapsack_Unbounded {
         int[][] dp=new int[n][capacity+1];
         for(int[] row:dp) Arrays.fill(row,-1);
         System.out.println(knapsack(n-1,capacity,wt,val,dp));
+        System.out.println(knapsackTab(n-1,capacity,wt,val,dp));
     }
 }
