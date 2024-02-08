d1220c599872f8e7e9ac63e397b31c10fe8d204f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-08 20:18:47 +0530

    Refactor --> Added Coin Change WIth Infinite Supply Problem

--- Java DSA BootCamp/Dynamic Programming/src/_23_Coin_Change_Infinite_Supply.java
+++ Java DSA BootCamp/Dynamic Programming/src/_23_Coin_Change_Infinite_Supply.java
@@ -1,11 +1,26 @@
 import java.util.Arrays;
 
 public class _23_Coin_Change_Infinite_Supply {
+    // Coin Change With Infinite Supply of Coins --> https://takeuforward.org/data-structure/coin-change-2-dp-22/
+
+    // Top - Down Approach --> Memoization
+    public static int count(int index,int target,int[] arr,int[][] dp){
+        if(index==0){
+            if(target%arr[0]==0) return 1;
+            else return 0;
+        }
+        if(dp[index][target]!=-1) return dp[index][target];
+        int notTake=count(index-1,target,arr,dp);
+        int take=0;
+        if(arr[index]<=target) take=count(index,target-arr[index],arr,dp);
+        return dp[index][target]=notTake+take;
+    }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3};
-        int T = 7;
+        int T = 4;
         int n = arr.length;
         int[][] dp = new int[n][T + 1];
         for (int[] row : dp) Arrays.fill(row, -1);
+        System.out.println(count(n-1,T,arr,dp));
     }
 }
