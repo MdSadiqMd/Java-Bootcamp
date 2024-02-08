8dab0b339d7fd6f47229c5b969404d8cc8b09a12
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-08 21:05:02 +0530

    Refactor --> Added Unbounded KnapSack Problem

--- Java DSA BootCamp/Dynamic Programming/src/_24_0_1_Knapsack_Unbounded.java
+++ Java DSA BootCamp/Dynamic Programming/src/_24_0_1_Knapsack_Unbounded.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 
 public class _24_0_1_Knapsack_Unbounded {
+    // 0/1 Knapsack Unbounded --> https://takeuforward.org/data-structure/unbounded-knapsack-dp-23/
+
+    // Top - Down Approach --> Memoization
     public static int knapsack(int index,int capacity,int[] wt,int[] val,int[][] dp){
         if(index==0) return (capacity/wt[0])*val[0];
         if(dp[index][capacity]!=-1) return dp[index][capacity];
@@ -8,6 +11,7 @@ public class _24_0_1_Knapsack_Unbounded {
         int take= (wt[index]<=capacity) ? val[index]+knapsack(index,capacity-wt[index],wt,val,dp) : 0;
         return dp[index][capacity]=Math.max(notTake,take);
     }
+    // Bottom - Up Approach --> Tabulation
     public static int knapsackTab(int index,int capacity,int[] wt,int[] val,int[][] dp){
         if(index==0) return (capacity/wt[0])*val[0];
         if(dp[index][capacity]!=-1) return dp[index][capacity];
@@ -20,10 +24,11 @@ public class _24_0_1_Knapsack_Unbounded {
         }
         return dp[index-1][capacity];
     }
+    // Space Optimization
     public static int knapsackSpace(int index,int capacity,int[] wt,int[] val){
         int[] curr=new int[capacity+1];
         for(int i=wt[0];i<=capacity;i++){
-            curr[i]=(int) (i/wt[0])*val[0];
+            curr[i]=((int)i/wt[0])*val[0];
         }
         for(int ind=1;ind<index;ind++){
             for(int cap=0;cap<=capacity;cap++){
