2073c22b6e5d87a836f50929e0bbc120dda6ddd6
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-09 21:41:27 +0530

    Refactor --> Added Rot Cutting Problem

--- Java DSA BootCamp/Dynamic Programming/src/_25_Rod_Cutting_Problem.java
+++ Java DSA BootCamp/Dynamic Programming/src/_25_Rod_Cutting_Problem.java
@@ -2,7 +2,15 @@ import java.util.Arrays;
 
 public class _25_Rod_Cutting_Problem {
     // Rod - Cutting problem --> https://takeuforward.org/data-structure/rod-cutting-problem-dp-24/
-
+    // Top - Down Approach --> Memoization
+    public static int count(int index,int capacity,int[] wt,int[] val,int[][] dp){
+        if(index==0) return (capacity/wt[0])*val[0];
+        if(dp[index][capacity]!=-1) return dp[index][capacity];
+        int notTake=count(index-1,capacity,wt,val,dp);
+        int take= Integer.MIN_VALUE;
+        if(wt[index]<=capacity) take=val[index]+count(index,capacity-wt[index],wt,val,dp);
+        return dp[index][capacity]=Math.max(notTake,take);
+    }
     public static void main(String[] args) {
         int[] wt = { 2, 4, 6 };
         int[] val = { 5, 11, 13 };
@@ -10,5 +18,6 @@ public class _25_Rod_Cutting_Problem {
         int n = wt.length;
         int[][] dp=new int[n][capacity+1];
         for(int[] row:dp) Arrays.fill(row,-1);
+        System.out.println(count(n-1,capacity,wt,val,dp));
     }
 }
