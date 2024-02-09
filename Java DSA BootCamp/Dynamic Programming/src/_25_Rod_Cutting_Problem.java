c849f01ca9eb41cac8df0da903a12b5f25ee37e5
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-09 21:46:18 +0530

    Refactor --> Added Rot Cutting Problem

--- Java DSA BootCamp/Dynamic Programming/src/_25_Rod_Cutting_Problem.java
+++ Java DSA BootCamp/Dynamic Programming/src/_25_Rod_Cutting_Problem.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class _25_Rod_Cutting_Problem {
     // Rod - Cutting problem --> https://takeuforward.org/data-structure/rod-cutting-problem-dp-24/
+
     // Top - Down Approach --> Memoization
     public static int count(int index,int capacity,int[] wt,int[] val,int[][] dp){
         if(index==0) return (capacity/wt[0])*val[0];
@@ -11,6 +12,20 @@ public class _25_Rod_Cutting_Problem {
         if(wt[index]<=capacity) take=val[index]+count(index,capacity-wt[index],wt,val,dp);
         return dp[index][capacity]=Math.max(notTake,take);
     }
+    // Bottom - Up Approach --> Tabulation
+    public static int countTab(int index,int capacity,int[] wt,int[] val,int[][] dp){
+        if(index==0) return (capacity/wt[0])*val[0];
+        if(dp[index][capacity]!=-1) return dp[index][capacity];
+        for(int ind=1;ind<index;ind++){
+            for(int T=0;T<=capacity;T++){
+                int notTake=dp[index-1][capacity];
+                int take= Integer.MIN_VALUE;
+                if(wt[ind]<=T) take=val[ind]+dp[ind][T-wt[ind]];
+                dp[ind][T]=Math.max(notTake,take);
+            }
+        }
+        return dp[index-1][capacity];
+    }
     public static void main(String[] args) {
         int[] wt = { 2, 4, 6 };
         int[] val = { 5, 11, 13 };
@@ -19,5 +34,6 @@ public class _25_Rod_Cutting_Problem {
         int[][] dp=new int[n][capacity+1];
         for(int[] row:dp) Arrays.fill(row,-1);
         System.out.println(count(n-1,capacity,wt,val,dp));
+        System.out.println(countTab(n-1,capacity,wt,val,dp));
     }
 }
