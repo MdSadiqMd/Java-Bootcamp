353f0a9ee5687b4188c94663af5ee1a528d30e12
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-11 20:07:28 +0530

    Refactor --> Added Longest Palindromic SubSequence Problem

--- Java DSA BootCamp/Dynamic Programming/src/_27_Longest_Palindromic_Subsequence.java
+++ Java DSA BootCamp/Dynamic Programming/src/_27_Longest_Palindromic_Subsequence.java
@@ -1,10 +1,60 @@
+import java.util.Arrays;
+
 public class _27_Longest_Palindromic_Subsequence {
     // Longest Palindromic Subsequence --> https://takeuforward.org/data-structure/longest-palindromic-subsequence-dp-28/
 
     // Top - Down Approach --> Memoization
-    public static int
+    public static int count(int i, int j, String s1, String s2, int[][] dp) {
+        if (i == 0 || j == 0) return 0;
+        if (dp[i][j] != 0) return dp[i][j];
+        if (s1.charAt(i - 1) == s2.charAt(j - 1)) return 1 + count(i - 1, j - 1, s1, s2, dp);
+        else return Math.max(count(i - 1, j, s1, s2, dp), count(i, j - 1, s1, s2, dp));
+    }
+    // Bottom - Up Approach --> Tabulation
+    public static int countTab(String s1, String s2, int[][] dp) {
+        int maxLen = 0;
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    maxLen = Math.max(maxLen, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return maxLen;
+    }
+    // Space Optimization
+    public static int countSpace(int n, int m, String s1, String s2) {
+        int[] prev = new int[m + 1];
+        int[] curr = new int[m + 1];
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    curr[j] = 1 + prev[j - 1];
+                    ans = Math.max(ans, curr[j]);
+                } else {
+                    curr[j] = 0;
+                }
+            }
+            prev = curr.clone();
+            Arrays.fill(curr, 0);
+        }
+        return ans;
+    }
+    public static void helper(String s1){
+        String s2=new StringBuilder(s1).reverse().toString();
+        int n=s1.length();
+        int m=s2.length();
+        int[][] dp=new int[n+1][m+1];
+        System.out.println(count(n - 1, m - 1, s1, s2, dp));
+        System.out.println(countTab(s1, s2, dp));
+        System.out.println(countSpace(n-1,m-1,s1,s2));
+    }
     public static void main(String[] args) {
-        String s="bbabcbcab";
-        int n=s.length();
+        String s1="bbabcbcab";
+        helper(s1);
     }
 }
