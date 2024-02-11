8e47cb3d074b14a3784da0b67ee2f847617dbe8f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-11 20:30:29 +0530

    Refactor --> Added Longest Palindromic SubSequence Problem

--- Java DSA BootCamp/Dynamic Programming/src/_28_Insertions_to_Make_String_Palindrome.java
+++ Java DSA BootCamp/Dynamic Programming/src/_28_Insertions_to_Make_String_Palindrome.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 
-public class _27_Longest_Palindromic_Subsequence {
+public class _28_Insertions_to_Make_String_Palindrome {
+    // Insertions to Make String Palindrome --> https://takeuforward.org/data-structure/minimum-insertions-to-make-string-palindrome-dp-29/
+
     // Top - Down Approach --> Memoization
     public static int count(int i, int j, String s1, String s2, int[][] dp) {
         if (i == 0 || j == 0) return 0;
@@ -17,15 +19,15 @@ public class _27_Longest_Palindromic_Subsequence {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                     maxLen = Math.max(maxLen, dp[i][j]);
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = 0;
                 }
             }
         }
         return maxLen;
     }
     // Space Optimization
-    public static int countSpace(int n,int m,String s1, String s2) {
-        int[] prev = new int[n+ 1];
+    public static int countSpace(int n, int m, String s1, String s2) {
+        int[] prev = new int[m + 1];
         int[] curr = new int[m + 1];
         int ans = 0;
         for (int i = 1; i <= n; i++) {
@@ -34,7 +36,7 @@ public class _27_Longest_Palindromic_Subsequence {
                     curr[j] = 1 + prev[j - 1];
                     ans = Math.max(ans, curr[j]);
                 } else {
-                    curr[j] = Math.max(curr[j - 1], prev[j]);
+                    curr[j] = 0;
                 }
             }
             prev = curr.clone();
@@ -42,17 +44,17 @@ public class _27_Longest_Palindromic_Subsequence {
         }
         return ans;
     }
-    public static void helper(String s1) {
-        String s2 = new StringBuilder(s1).reverse().toString();
-        int n = s1.length();
-        int m = s2.length();
-        int[][] dp = new int[n + 1][m + 1];
-        System.out.println(count(n, m, s1, s2, dp));
-        System.out.println(countTab(s1, s2, dp));
-        System.out.println(countSpace(n,m,s1, s2));
+    public static void helper(String s1){
+        String s2=new StringBuilder(s1).reverse().toString();
+        int n=s1.length();
+        int m=s2.length();
+        int[][] dp=new int[n+1][m+1];
+        System.out.println(n-count(n - 1, m - 1, s1, s2, dp));
+        System.out.println(n-countTab(s1, s2, dp));
+        System.out.println(n-countSpace(n-1,m-1,s1,s2));
     }
     public static void main(String[] args) {
-        String s1 = "bbabcbcab";
+        String s1="abcaa";
         helper(s1);
     }
 }
