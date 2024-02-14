a5158da3f53081fba2570d163236af44de489609
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-14 22:17:13 +0530

    Refactor --> Construct Binary Tree using Inorder and PreOrder

--- Java DSA BootCamp/Dynamic Programming/src/_32_Edit_Distance_Insertion_Deletion_Replace_Character.java
+++ Java DSA BootCamp/Dynamic Programming/src/_32_Edit_Distance_Insertion_Deletion_Replace_Character.java
@@ -1,5 +1,18 @@
-public class _32_Edit_Distance_Insertion_Deletion_Replace_Character {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class _32_Edit_Distance_Insertion_Deletion_Replace_Character {
+    // Edit Distance Insertion Deletion and Replace Character --> https://takeuforward.org/data-structure/edit-distance-dp-33/
+
+    // Top - Down Approach --> Memoization
+    public static int count(int n,int m,String s1,String s2,int[][] dp){
+
+    }
+    public static void main(String[] args) {
+        String s1 = "horse";
+        String s2 = "ros";
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n][m];
+        for (int[] row : dp) Arrays.fill(row, -1);
     }
 }
--- Java DSA BootCamp/Trees/src/_30_Construct_Binary_Tree.java
+++ Java DSA BootCamp/Trees/src/_30_Construct_Binary_Tree.java
@@ -9,6 +9,7 @@ public class _30_Construct_Binary_Tree {
             left = right = null;
         }
     }
+    // Constructing Binary Tree with PreOrder and InOrder
     public static TreeNode constructTree(int[] preOrder, int[] inOrder, HashMap<Integer, Integer> indexMap, int preOrderStart, int preOrderEnd, int inOrderStart, int inOrderEnd) {
         if (preOrderStart > preOrderEnd || inOrderStart > inOrderEnd) return null;
         int rootData = preOrder[preOrderStart];
@@ -19,9 +20,20 @@ public class _30_Construct_Binary_Tree {
         root.right = constructTree(preOrder, inOrder, indexMap, preOrderStart + leftSubTreeSize + 1, preOrderEnd, rootIndex + 1, inOrderEnd);
         return root;
     }
-
+    // Constructing Binary Tree with PostOrder and InOrder
+    public static TreeNode constructTreePostOrder(int[] postOrder, int[] inOrder, HashMap<Integer, Integer> indexMap, int postOrderStart, int postOrderEnd, int inOrderStart, int inOrderEnd) {
+        if (postOrderStart > postOrderEnd || inOrderStart > inOrderEnd) return null;
+        int rootData = postOrder[postOrderEnd];
+        int rootIndex = indexMap.get(rootData);
+        TreeNode root = new TreeNode(rootData);
+        int rightSubTreeSize = inOrderEnd - rootIndex;
+        root.left = constructTreePostOrder(postOrder, inOrder, indexMap, postOrderStart, postOrderEnd - rightSubTreeSize - 1, inOrderStart, rootIndex - 1);
+        root.right = constructTreePostOrder(postOrder, inOrder, indexMap, postOrderEnd - rightSubTreeSize, postOrderEnd - 1, rootIndex + 1, inOrderEnd);
+        return root;
+    }
     public static void main(String[] args) {
         int[] preOrder = { 3, 9, 20, 15, 7 };
+        int[] postOrder = { 9, 15, 7, 20, 3 };
         int[] inOrder = { 9, 3, 15, 20, 7 };
         HashMap<Integer, Integer> indexMap = new HashMap<>();
         for (int i = 0; i < inOrder.length; i++) {
@@ -29,8 +41,12 @@ public class _30_Construct_Binary_Tree {
         }
         int n = preOrder.length - 1;
         int m = inOrder.length - 1;
+        int o = postOrder.length - 1;
         TreeNode root = constructTree(preOrder, inOrder, indexMap, 0, n, 0, m);
+        TreeNode root1 = constructTreePostOrder(postOrder, inOrder, indexMap, 0, n, 0, o);
         printInOrder(root);
+        System.out.println();
+        printInOrder(root1);
     }
     static void printInOrder(TreeNode root) {
         if (root == null) return;
