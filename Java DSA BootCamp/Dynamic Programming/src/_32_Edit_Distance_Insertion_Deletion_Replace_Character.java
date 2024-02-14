240df36a967cdc88f6567fabd16b795a54c27ebf
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-14 22:42:27 +0530

    Refactor --> Added Edit Distance Problem

--- Java DSA BootCamp/Dynamic Programming/src/_32_Edit_Distance_Insertion_Deletion_Replace_Character.java
+++ Java DSA BootCamp/Dynamic Programming/src/_32_Edit_Distance_Insertion_Deletion_Replace_Character.java
@@ -13,27 +13,43 @@ public class _32_Edit_Distance_Insertion_Deletion_Replace_Character {
     }
     // Bottom - Up Approach --> Tabulation
     public static int countTab(int n,int m,String s1,String s2,int[][] dp){
-        if(n<0) return n+1;
-        if(m<0) return m+1;
+        if(n<0) return m+1;
+        if(m<0) return n+1;
         if(dp[n][m]!=-1) return dp[n][m];
         for (int i = 0; i <= n; i++) dp[i][0] = i;
         for (int j = 0; j <= m; j++) dp[0][j] = j;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s1.charAt(i)==s2.charAt(j)) return dp[i][j]=dp[i-1][j-1];
-                else return dp[i][j]=Math.min(1+dp[i-1][j-1],Math.min(1+dp[i-1][j],1+dp[i][j-1]));
+                if(s1.charAt(i-1)==s2.charAt(j-1)) dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=Math.min(1+dp[i-1][j-1],Math.min(1+dp[i-1][j],1+dp[i][j-1]));
             }
         }
         return dp[n][m];
+    }
+    // Space Optimization
+    public static int countSpace(int n,int m,String s1,String s2){
+        int[] prev=new int[m+1];
+        int[] curr=new int[m+1];
+        for (int i = 0; i <= m; i++) prev[i]= i;
+        for(int i=1;i<=n;i++){
+            curr[0]=i;
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)) curr[j]=prev[j-1];
+                else curr[j]=1+Math.min(curr[j-1],Math.min(prev[j-1],prev[j]));
+            }
+            prev=curr.clone();
+        }
+        return prev[m];
     }
     public static void main(String[] args) {
         String s1 = "horse";
         String s2 = "ros";
         int n = s1.length();
         int m = s2.length();
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n+1][m+1];
         for (int[] row : dp) Arrays.fill(row, -1);
         System.out.println(count(n-1,m-1,s1,s2,dp));
-        System.out.println(countTab(n-1,m-1,s1,s2,dp));
+        System.out.println(countTab(n,m,s1,s2,dp));
+        System.out.println(countSpace(n,m,s1,s2));
     }
 }
