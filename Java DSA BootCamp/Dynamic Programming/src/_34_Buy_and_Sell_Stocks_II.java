07b235ec0e1672fc30f671cb93f0bdd8f992b347
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-18 21:54:43 +0530

    Refactor --> Added Buy and Sell Stocks II Problem

--- Java DSA BootCamp/Dynamic Programming/src/_34_Buy_and_Sell_Stocks_II.java
+++ Java DSA BootCamp/Dynamic Programming/src/_34_Buy_and_Sell_Stocks_II.java
@@ -13,16 +13,33 @@ public class _34_Buy_and_Sell_Stocks_II {
         return dp[index][buy ? 1 : 0] = profit;
     }
     // Bottom - Up Approach --> Tabulation
-    public static long profitTab(int index, int n, boolean buy, long[] arr, long[][] dp){
-        dp[n][0]=dp[n][1]=0;
-        long profit = 0;
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<=1;j++){
-                if (buy) profit = Math.max(-arr[i] + dp[i+1][1],dp[i+1][0]);
-                else profit = Math.max(arr[i] + dp[i+1][0],dp[i+1][1]);
+    public static long profitTab(int index, int n, long[] arr, long[][] dp) {
+        dp[n][0] = dp[n][1] = 0;
+        long profit=0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy == 1) profit = Math.max(dp[i + 1][0] - arr[i], dp[i + 1][1]);
+                if (buy == 0) profit = Math.max(dp[i + 1][0], dp[i + 1][1] + arr[i]);
+                else dp[i][buy] = profit;
             }
         }
-        return dp[index][buy ? 1 : 0] = profit;
+        return Math.max(dp[index][0], dp[index][1]);
+    }
+    // Space Optimization
+    public static long profitSpace(int n,long[] arr) {
+        long[] ahead = new long[2];
+        long[] cur = new long[2];
+        ahead[0] = ahead[1] = 0;
+        long profit = 0;
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy == 0) profit = Math.max(ahead[0], -arr[ind] + ahead[1]);
+                if (buy == 1) profit = Math.max(ahead[1], arr[ind] + ahead[0]);
+                cur[buy] = profit;
+            }
+            System.arraycopy(cur, 0, ahead, 0, 2);
+        }
+        return cur[0];
     }
     public static void main(String[] args) {
         int n = 6;
@@ -30,6 +47,7 @@ public class _34_Buy_and_Sell_Stocks_II {
         long[][] dp = new long[n + 1][2];
         for (long[] row : dp) Arrays.fill(row, -1);
         System.out.println(profit(0, n, true, arr, dp));
-        System.out.println(profitTab(0, n, true, arr, dp));
+        System.out.println(profitTab(0, n, arr, dp));
+        System.out.println(profitSpace(n,arr));
     }
 }
