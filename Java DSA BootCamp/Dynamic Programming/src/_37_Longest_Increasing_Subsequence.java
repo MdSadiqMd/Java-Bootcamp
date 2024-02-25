710cf4e8d2fdec1f448e5f8b30f1a40da8d2f71e
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-25 22:01:26 +0530

    Refactor --> Added Longest Increasing Sub Sequence problem

--- Java DSA BootCamp/Dynamic Programming/src/_37_Longest_Increasing_Subsequence.java
+++ Java DSA BootCamp/Dynamic Programming/src/_37_Longest_Increasing_Subsequence.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class _37_Longest_Increasing_Subsequence {
-    // Longest Increasing Sub-Sequence --> https://takeuforward.org/data-structure/longest-increasing-subsequence-dp-41/ ,
+    // Longest Increasing Sub-Sequence --> https://takeuforward.org/data-structure/longest-increasing-subsequence-dp-41/, https://takeuforward.org/data-structure/printing-longest-increasing-subsequence-dp-42/,
 
     // Top - Down Approach --> Memoization
     public static int count(int[] arr,int n,int curr_ind,int prev_ind,int[][] dp){
@@ -40,6 +41,38 @@ public class _37_Longest_Increasing_Subsequence {
         }
         return curr[0];
     }
+    // Printing The Longest Increasing SubSequence
+    public static int countPrint(int[] arr,int n){
+        int[] dp=new int[n];
+        Arrays.fill(dp,1);
+        int[] hash=new int[n];
+        Arrays.fill(hash,1);
+        for(int i=0; i<=n-1; i++){
+            hash[i] = i;
+            for(int prev_index = 0; prev_index <=i-1; prev_index ++){
+                if(arr[prev_index]<arr[i] && 1 + dp[prev_index] > dp[i]){
+                    dp[i] = 1 + dp[prev_index];
+                    hash[i] = prev_index;
+                }
+            }
+        }
+        int ans = -1;
+        int lastIndex =-1;
+        for(int i=0; i<=n-1; i++){
+            if(dp[i]> ans){
+                ans = dp[i];
+                lastIndex = i;
+            }
+        }
+        ArrayList<Integer> temp=new ArrayList<>();
+        temp.add(arr[lastIndex]);
+        while(hash[lastIndex] != lastIndex){ // till not reach the initialization value
+            lastIndex = hash[lastIndex];
+            temp.add(arr[lastIndex]);
+        }
+        for(int i=temp.size()-1; i>=0; i--) System.out.print(temp.get(i)+" ");
+        return ans;
+    }
     public static void main(String[] args) {
         int[] arr={10, 9, 2, 5, 3, 7, 101, 18};
         int n=arr.length;
@@ -48,5 +81,6 @@ public class _37_Longest_Increasing_Subsequence {
         System.out.println(count(arr,n,0,-1,dp));
         System.out.println(countTab(arr,n,dp));
         System.out.println(countSpace(arr,n));
+        System.out.println(countPrint(arr,n));
     }
 }
