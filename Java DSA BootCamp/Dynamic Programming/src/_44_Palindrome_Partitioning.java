c1dfd03fe6956f4219bc73c89a4d0c98d58d935c
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-05 19:28:49 +0530

    Refactor --> Added Palindrome Parititioning II Problem

--- Java DSA BootCamp/Dynamic Programming/src/_44_Palindrome_Partitioning.java
+++ Java DSA BootCamp/Dynamic Programming/src/_44_Palindrome_Partitioning.java
@@ -15,6 +15,20 @@ public class _44_Palindrome_Partitioning {
         }
         return dp[ind]=cost;
     }
+    // Bottom - Up Approach --> Tabulation
+    public static int countTab(String str,int ind ,int n,int[] dp){
+        if(ind==n) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int cost=Integer.MAX_VALUE;
+        for(int i=ind;i<n;i++){
+            for(int j=i;j>=0;j--){
+                if(isPalindrome(str,ind,i)){
+                    cost=Math.min(cost,1+dp[j+1]);
+                }
+            }
+        }
+        return dp[ind]=cost;
+    }
     public static boolean isPalindrome(String str,int i,int j){
         while(i<j){
             if(str.charAt(i)!=str.charAt(j)) return false;
@@ -29,5 +43,6 @@ public class _44_Palindrome_Partitioning {
         int[] dp=new int[n+1];
         Arrays.fill(dp,-1);
         System.out.println(count(str,0,n,dp)-1); // "-1" is to remove the last partition at the end
+        System.out.println(countTab(str,0,n,dp)-1);
     }
 }
