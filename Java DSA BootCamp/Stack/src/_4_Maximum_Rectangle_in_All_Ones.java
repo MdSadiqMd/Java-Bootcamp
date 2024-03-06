f5810c5c64f24fd352f6342918c326def7841ea2
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-06 19:48:42 +0530

    Refactor --> Added Rectangles with All 1's

---
+++ Java DSA BootCamp/Stack/src/_4_Maximum_Rectangle_in_All_Ones.java
@@ -0,0 +1,6 @@
+public class _4_Maximum_Rectangle_in_All_Ones {
+    // Maximum Rectangle with All 1's --> https://www.geeksforgeeks.org/maximum-size-rectangle-binary-sub-matrix-1s/
+    public static void main(String[] args) {
+
+    }
+}
