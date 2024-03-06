281cf77fef0ba6ca8e04ddaf7645dbae102c8b60
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-06 19:37:44 +0530

    Refactor --> Added Largest Reactangle in Histogram Problem

--- Java DSA BootCamp/Stack/src/_3_Largest_Rectangle_in_Histogram.java
+++ Java DSA BootCamp/Stack/src/_3_Largest_Rectangle_in_Histogram.java
@@ -1,5 +1,23 @@
-public class _3_Largest_Rectangle_in_Histogram {
-    public static void main(String[] args) {
+import java.util.Stack;
 
+public class _3_Largest_Rectangle_in_Histogram {
+    public static int area(int[] arr){
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        int width, height;
+        int n = arr.length;
+        for (int i = 0; i <= n; i++) {
+            while (!stack.isEmpty() && (i == n || arr[stack.peek()] >= arr[i])) {
+                height = arr[stack.pop()];
+                width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                max = Math.max(max, width * height);
+            }
+            stack.push(i);
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] arr = {6, 7, 5, 2, 4, 5, 9, 3};
+        System.out.println(area(arr));
     }
 }
