21538c808be53e49b80550693788e64af037ff39
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-06 19:46:45 +0530

    .gitIgnore

--- Java DSA BootCamp/Dynamic Programming/src/_46_Maximum_Rectangles_with_All_Ones.java
+++
@@ -1,7 +0,0 @@
-public class _46_Maximum_Rectangles_with_All_Ones {
-    // Maximum Rectangle with All 1's --> https://www.geeksforgeeks.org/maximum-size-rectangle-binary-sub-matrix-1s/
-
-    public static void main(String[] args) {
-
-    }
-}
