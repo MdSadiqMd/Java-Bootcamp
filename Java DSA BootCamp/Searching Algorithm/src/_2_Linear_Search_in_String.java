62e63a80f25df007b44e3d831dbf1176446e33cf
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-08 17:19:16 +0530

    Refactor --> Added Linear Search on Strings

---
+++ Java DSA BootCamp/Searching Algorithm/src/_2_Linear_Search_in_String.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class _2_Linear_Search_in_String {
+    public static void LinearSearch(String name, char target){
+        int count=-1;
+        if(name.length()==0)System.out.println("String is empty");
+        for(int i=0;i<name.length();i++){
+            if(target==name.charAt(i)){
+                count=i;
+                break;
+            }
+        }
+        if(count==-1)System.out.println("Element is not present in the Array");
+        else System.out.println("Element is present at Index "+count +"\n"+"Element is present in the place "+(count+1));
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String");
+        String name=sc.nextLine();
+        System.out.println("Enter the Target Letter to be Searched :");
+        char target=sc.next().charAt(0);
+        LinearSearch(name,target);
+    }
+}
