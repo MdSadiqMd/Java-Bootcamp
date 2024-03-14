af9f9a90ba22eddb1f738614c5961ffc7f63ab3c
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-14 22:54:42 +0530

    Refactor --> Added Nth Root of a Number

--- Java DSA BootCamp/Searching Algorithm/src/_14_Nth_Root_of_a_Number.java
+++ Java DSA BootCamp/Searching Algorithm/src/_14_Nth_Root_of_a_Number.java
@@ -11,18 +11,26 @@ public class _14_Nth_Root_of_a_Number {
         return 0;
     }
     public static int search(int n, int m) {
-        int low = 1, high = m;
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        int l = 1, h = m;
+        while (l <= h) {
+            int mid = (l + h) / 2;
             int val = helper(mid, n, m);
             if (val == 1) return mid;
-            else if (val == 0) low = mid + 1;
-            else high = mid - 1;
+            else if (val == 0) l = mid + 1;
+            else h = mid - 1;
         }
         return -1;
     }
-
+    public static int searchRecursive(int n,int m,int l,int h){
+        if(l>h) return -1;
+        int mid = (l + h) / 2;
+        int val = helper(mid, n, m);
+        if (val == 1) return mid;
+        else if (val == 0) return searchRecursive(n,m,mid+1,h);
+        else return searchRecursive(n,m,l,mid-1);
+    }
     public static void main(String[] args) {
         System.out.println(search(3,27));
+        System.out.println(searchRecursive(3,27,1,27));
     }
 }
