68196e76b4f81e7682d407a583f1433c6d1fd51e
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-18 21:58:48 +0530

    Refactor --> Added Smallest Divisor in Given Threshold

--- Java DSA BootCamp/Searching Algorithm/src/_17_Find_Smallest_Divisor_Given_Threshold.java
+++ Java DSA BootCamp/Searching Algorithm/src/_17_Find_Smallest_Divisor_Given_Threshold.java
@@ -1,4 +1,5 @@
 public class _17_Find_Smallest_Divisor_Given_Threshold {
+    // Find Smallest Divisor with Given Threshold --> https://takeuforward.org/arrays/find-the-smallest-divisor-given-a-threshold/
     public static void main(String[] args) {
 
     }
