76fb431bd185766fad5796d8a51d4299e6f246ea
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-19 19:48:27 +0530

    Refactor --> Added Capacity to Ship Packages in D Days

---
+++ Java DSA BootCamp/Searching Algorithm/src/_18_Capacity_to_Ship_Packages_in_D_Days.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class _18_Capacity_to_Ship_Packages_in_D_Days {
+    public static int helper(int[] arr,int n,int k){
+        int days=1,load=0;
+        for(int i=0;i<=n-1;i++){
+            if(load+arr[i]>k){
+                days=days+1;
+                load=arr[i];
+            }
+            else load+=arr[i];
+        }
+        return days;
+    }
+    public static int capacity(int[] arr,int n,int k,int l,int h){
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(helper(arr,n,mid)<=k) h=mid-1;
+            else l=mid+1;
+        }
+        return l;
+    }
+    public static void main(String[] args) {
+        int[] arr={5, 4, 5, 2, 3, 4, 5, 6};
+        Arrays.sort(arr);
+        int n=arr.length,k=5,l=arr[n-1],sum=0;
+        for(int i=0;i<=n-1;i++) sum+=arr[i];
+        int h=sum;
+        System.out.println(capacity(arr,n,k,l,h));
+    }
+}
