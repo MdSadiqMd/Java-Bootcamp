7cf5187ab3acdfc672af2f81074e64f1392c9a31
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-20 17:20:39 +0530

    Refactor --> Added Kth Missing Positive Number problem

--- Java DSA BootCamp/Searching Algorithm/src/_19_Kth_Missing_Positive_Number.java
+++ Java DSA BootCamp/Searching Algorithm/src/_19_Kth_Missing_Positive_Number.java
@@ -9,9 +9,18 @@ public class _19_Kth_Missing_Positive_Number {
         }
         return l+k;
     }
+    public static int findRecursive(int[] arr,int n,int k,int l,int h){
+        if(l<=h){
+            int mid=l+(h-l)/2;
+            if(arr[mid]-(mid+1)>k) return findRecursive(arr,n,k,l,mid-1);
+            else return findRecursive(arr,n,k,mid+1,h);
+        }
+        return l+k;
+    }
     public static void main(String[] args) {
         int[] arr={2,3,4,6,9,11};
         int n=arr.length,k=5;
         System.out.println(find(arr,n,k));
+        System.out.println(findRecursive(arr,n,k,1,n-2));
     }
 }
