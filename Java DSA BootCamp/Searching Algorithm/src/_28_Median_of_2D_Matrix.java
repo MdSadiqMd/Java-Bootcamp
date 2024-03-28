77767aae263d5d2c2ea0a41f20507830b027f96d
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-28 19:44:00 +0530

    .gitIgnore

---
+++ Java DSA BootCamp/Searching Algorithm/src/_28_Median_of_2D_Matrix.java
@@ -0,0 +1,10 @@
+public class _28_Median_of_2D_Matrix {
+    // Median in Row Wise Sorted Array --> https://takeuforward.org/data-structure/median-of-row-wise-sorted-matrix/
+    public static int median(int[][] arr,int row,int col){
+
+    }
+    public static void main(String[] args) {
+        int[][] arr={{1, 3, 8},{2, 3, 4},{1, 2, 5}};
+        int row=3,col=3;
+    }
+}
