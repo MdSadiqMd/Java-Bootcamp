6b2b35db66f2f86a2611ff4f3802af6db5b96629
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-28 20:17:30 +0530

    Refactor --> Added Median of 2D Matrix

--- Java DSA BootCamp/Searching Algorithm/src/_28_Median_of_2D_Matrix.java
+++ Java DSA BootCamp/Searching Algorithm/src/_28_Median_of_2D_Matrix.java
@@ -1,10 +1,28 @@
 public class _28_Median_of_2D_Matrix {
     // Median in Row Wise Sorted Array --> https://takeuforward.org/data-structure/median-of-row-wise-sorted-matrix/
-    public static int median(int[][] arr,int row,int col){
-
+    public static int helper(int[] arr,double mid,int n){
+        int l=0,h=n-1;
+        while(l<=h){
+            int mid1=(l+h)>>1;
+            if(arr[mid1]<=mid) l=mid1+1;
+            else h=mid1-1;
+        }
+        return l;
+    }
+    public static double median(int[][] arr,int row,int col){
+        double l=1,h=Math.pow(10,9);
+        while(l<=h){
+            double mid=l+(h-l)/2;
+            int count=0;
+            for(int i=0;i<row;i++) count+=helper(arr[i],mid,col);
+            if(count<=row*col/2) l=mid+1;
+            else h=mid-1;
+        }
+        return l;
     }
     public static void main(String[] args) {
         int[][] arr={{1, 3, 8},{2, 3, 4},{1, 2, 5}};
         int row=3,col=3;
+        System.out.println(Math.ceil(median(arr,row,col)));
     }
 }
