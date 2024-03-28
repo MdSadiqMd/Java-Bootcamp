b2faa93a2a0e7c9ec00af45cf5371ca0fcb787dd
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-28 20:23:05 +0530

    Refactor --> Added Median of 2D Matrix

--- Java DSA BootCamp/Searching Algorithm/src/_28_Median_of_2D_Matrix.java
+++ Java DSA BootCamp/Searching Algorithm/src/_28_Median_of_2D_Matrix.java
@@ -20,9 +20,20 @@ public class _28_Median_of_2D_Matrix {
         }
         return l;
     }
+    public static double medianRecursive(int[][] arr,int row,int col,double l,double h){
+        if(l<=h){
+            double mid=l+(h-l)/2;
+            int count=0;
+            for(int i=0;i<row;i++) count+=helper(arr[i],mid,col);
+            if(count<=row*col/2) return medianRecursive(arr,row,col,mid+1,h);
+            else return medianRecursive(arr,row,col,l,mid-1);
+        }
+        return l;
+    }
     public static void main(String[] args) {
         int[][] arr={{1, 3, 8},{2, 3, 4},{1, 2, 5}};
         int row=3,col=3;
         System.out.println(Math.ceil(median(arr,row,col)));
+        System.out.println(Math.floor(medianRecursive(arr,row,col,0,Math.pow(10,9))));
     }
 }
