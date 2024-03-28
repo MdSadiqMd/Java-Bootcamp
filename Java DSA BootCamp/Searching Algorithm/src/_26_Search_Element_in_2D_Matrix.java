4a922e4cdacbb75e27318e3dc6d9991f63fbdc87
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-28 19:16:59 +0530

    Refactor --> Added Serach Element in 2D matrix Problem

--- Java DSA BootCamp/Searching Algorithm/src/_26_Search_Element_in_2D_Matrix.java
+++ Java DSA BootCamp/Searching Algorithm/src/_26_Search_Element_in_2D_Matrix.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class _26_Search_Element_in_2D_Matrix {
-    // Search Element in 2D Matrix --> https://takeuforward.org/data-structure/search-in-a-sorted-2d-matrix/
+    // Search Element in 2D Matrix --> https://takeuforward.org/data-structure/search-in-a-sorted-2d-matrix/ , https://takeuforward.org/arrays/search-in-a-row-and-column-wise-sorted-matrix/
     public static boolean search(ArrayList<ArrayList<Integer>> arr,int k){
         int n=arr.size(),m=arr.get(0).size(),l=0,h=n-1;
         for(int i=0;i<n;i++){
@@ -31,6 +31,15 @@ public class _26_Search_Element_in_2D_Matrix {
         }
         return false;
     }
+    public static int[][] searchElement(ArrayList<ArrayList<Integer>> arr,int k){
+        int n=arr.size(),row=0,col=n-1;
+        while(row<n && col>=0){
+            if(arr.get(row).get(col)==k) return new int[][]{{row,col}};
+            else if(arr.get(row).get(col)<k) row++;
+            else col--;
+        }
+        return new int[][]{{-1,-1}};
+    }
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> arr=new ArrayList<>();
         arr.add(new ArrayList<>(Arrays.asList(1,2,3,4)));
@@ -39,5 +48,6 @@ public class _26_Search_Element_in_2D_Matrix {
         int k=6;
         System.out.println(search(arr,k));
         System.out.println(searchOptimize(arr,k));
+        System.out.println(Arrays.deepToString(searchElement(arr, k)));
     }
 }
