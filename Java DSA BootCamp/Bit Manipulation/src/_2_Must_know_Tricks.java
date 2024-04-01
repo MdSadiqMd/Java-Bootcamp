1363fe10286ccc7caec2d08afd900fd8eaf6cd21
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-01 20:41:34 +0530

    Refactor --> Added Clear and Setting Bit's

--- Java DSA BootCamp/Bit Manipulation/src/_2_Must_know_Tricks.java
+++ Java DSA BootCamp/Bit Manipulation/src/_2_Must_know_Tricks.java
@@ -7,13 +7,20 @@ public class _2_Must_know_Tricks {
         a=a^b;
         return new int[]{a,b};
     }
-    public static int checkIthBitSet(int a){
-        int i=2;
+    public static int checkIthBitSet(int a,int i) {
         return a&(1<<i);
+    }
+    public static int setIthBit(int a,int i){
+        return a|(1<<i);
+    }
+    public static int clearIthBit(int a,int i){
+        return a&~(1<<i);
     }
     public static void main(String[] args) {
         int a=1010,b=1101;
         System.out.println(Arrays.toString(swap(a,b)));
-        System.out.println(checkIthBitSet(a));
+        System.out.println(Integer.toBinaryString(checkIthBitSet(a,2)));
+        System.out.println(Integer.toBinaryString(setIthBit(a,2)));
+        System.out.println(Integer.toBinaryString(clearIthBit(a,2)));
     }
 }
