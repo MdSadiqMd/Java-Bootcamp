4e5606cf09f51042772dc984477d4b7df8056cbd
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-01 21:08:27 +0530

    Refactor --> Added toggling and removing set Bits

--- Java DSA BootCamp/Bit Manipulation/src/_2_Must_know_Tricks.java
+++ Java DSA BootCamp/Bit Manipulation/src/_2_Must_know_Tricks.java
@@ -16,11 +16,33 @@ public class _2_Must_know_Tricks {
     public static int clearIthBit(int a,int i){
         return a&~(1<<i);
     }
+    public static int toggle(int a,int i){
+        return a^(1<<i);
+    }
+    public static int removeLastSetBit(int a){
+        return a&(a-1);
+    }
+    public static boolean powOf2(int a){
+        return (a&(a-1))==0;
+    }
+    public static int countSetBits(int a){
+        int count=0;
+        while(a>1){
+            count+=a&1;
+            a=a>>1;
+        }
+        if(a==1) count+=1;
+        return count;
+    }
     public static void main(String[] args) {
-        int a=1010,b=1101;
+        int a=1010,b=1101,c=4;
         System.out.println(Arrays.toString(swap(a,b)));
         System.out.println(Integer.toBinaryString(checkIthBitSet(a,2)));
         System.out.println(Integer.toBinaryString(setIthBit(a,2)));
         System.out.println(Integer.toBinaryString(clearIthBit(a,2)));
+        System.out.println(Integer.toBinaryString(toggle(a,2)));
+        System.out.println(Integer.toBinaryString(removeLastSetBit(a)));
+        System.out.println(powOf2(a));
+        System.out.println(countSetBits(a));
     }
 }
