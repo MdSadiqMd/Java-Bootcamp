620cc566d2750eff820a93a7aabe2fc8bac9a2cb
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-02 21:03:22 +0530

    Refactor --> Added bit Flips to Convert Number Problem

---
+++ Java DSA BootCamp/Bit Manipulation/src/_3_Problems_on_Bit_Manipulation.java
@@ -0,0 +1,14 @@
+public class _3_Problems_on_Bit_Manipulation {
+    public static int bitFlipsToConvertNumber(int a,int b){
+        int ans=a^b;
+        int count=0;
+        for(int i=0;i<=3;i++){
+            if((ans&(1<<i))!=0) count=count+1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int a=1110,b=1010;
+        System.out.println(Integer.toBinaryString(bitFlipsToConvertNumber(a,b)));
+    }
+}
