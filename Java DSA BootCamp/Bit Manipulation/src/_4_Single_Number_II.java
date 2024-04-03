add3cc948e0af5996c8314ed05a8fd626687bb96
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-03 22:14:27 +0530

    Refactor --> Added Single number II porblem

--- Java DSA BootCamp/Bit Manipulation/src/_4_Single_Number_II.java
+++ Java DSA BootCamp/Bit Manipulation/src/_4_Single_Number_II.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class _4_Single_Number_II {
     public static int search(int[] arr){
-        int ans=0,count=0;
+        int count,ans=0;
         for(int i=0;i<=31;i++){
+            count=0;
             for(int j=0;j<=arr.length-1;j++){
                 if((arr[j]&(1<<i))!=0) count++;
             }
@@ -9,8 +12,16 @@ public class _4_Single_Number_II {
         }
         return ans;
     }
+    public static int searchOptimized(int[] arr){
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length-1;i+=3){
+            if(arr[i]!=arr[i+1]) return arr[i];
+        }
+        return arr[arr.length-1];
+    }
     public static void main(String[] args) {
         int[] arr={5,5,5,6,4,4,4};
         System.out.println(search(arr));
+        System.out.println(searchOptimized(arr));
     }
 }
