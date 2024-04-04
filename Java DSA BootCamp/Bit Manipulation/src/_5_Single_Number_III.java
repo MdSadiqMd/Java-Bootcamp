c22e1fd0bb5d23e7fadf6b431674dfcea90458f0
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-04 19:31:40 +0530

    Refactor --> Added SIngle Number III Problem

---
+++ Java DSA BootCamp/Bit Manipulation/src/_5_Single_Number_III.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class _5_Single_Number_III {
+    public static int[] search(int[] arr){
+        int xor=0,b1=0,b2=0;
+        for(int i=0;i<=arr.length-1;i++) xor=xor^arr[i];
+        int rightMostBit=(xor^xor-1)&xor;
+        for(int i=0;i<=arr.length-1;i++){
+            if((arr[i]&rightMostBit)!=0) b1=b1^arr[i];
+            else b2=b2^arr[i];
+        }
+        return new int[]{b1,b2};
+    }
+    public static void main(String[] args) {
+        int[] arr={1,1,2,2,3,3,4,5,5,6};
+        System.out.println(Arrays.toString(search(arr)));
+    }
+}
