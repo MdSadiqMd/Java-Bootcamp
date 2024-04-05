e571c860e9c5c8f113601b766f8c1f7801943dc8
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-05 20:05:07 +0530

    Refactor --> Added Divisors of a Number Problem

--- Java DSA BootCamp/Mathematics/src/_1_Square_Root_Method.java
+++ Java DSA BootCamp/Mathematics/src/_1_Square_Root_Method.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class _1_Square_Root_Method {
     // Prime or Not, GCD of Two Numbers --> https://takeuforward.org/data-structure/check-if-a-number-is-prime-or-not/, https://takeuforward.org/data-structure/find-gcd-of-two-numbers/
+    public static ArrayList<Integer> divisors(int n){
+        ArrayList<Integer> ans=new ArrayList<>();
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0) {
+                ans.add(i);
+                if((n/i)!=i) ans.add(n/i);
+            }
+        }
+        return ans;
+    }
     public static boolean prime(int n){
         int count=0;
         for(int i=1;i*i<n;i++){
@@ -20,7 +32,8 @@ public class _1_Square_Root_Method {
     }
     public static void main(String[] args) {
         int n=20,m=40;
-        System.out.println(gcd(n,m));
+        System.out.println(divisors(n));
         System.out.println(prime(n));
+        System.out.println(gcd(n,m));
     }
 }
