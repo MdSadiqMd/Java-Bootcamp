aa585199eec0a82ae2b6889f4f083679513278b1
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-05 19:58:15 +0530

    Refactor --> Added gcd Problem

--- Java DSA BootCamp/Mathematics/src/_1_Square_Root_Method.java
+++ Java DSA BootCamp/Mathematics/src/_1_Square_Root_Method.java
@@ -1,4 +1,13 @@
 public class _1_Square_Root_Method {
+    // GCD of Two Numbers, Prime or Not --> https://takeuforward.org/data-structure/find-gcd-of-two-numbers/, https://takeuforward.org/data-structure/check-if-a-number-is-prime-or-not/
+    public static int gcd(int n,int m){
+        while(n>0 && m>0){
+            if(n>m) n=n%m;
+            else m=m%n;
+        }
+        if(n==0) return m;
+        else return n;
+    }
     public static boolean prime(int n){
         int count=0;
         for(int i=1;i*i<n;i++){
@@ -10,7 +19,8 @@ public class _1_Square_Root_Method {
         return count == 2;
     }
     public static void main(String[] args) {
-        int n=2;
+        int n=20,m=40;
+        System.out.println(gcd(n,m));
         System.out.println(prime(n));
     }
 }
