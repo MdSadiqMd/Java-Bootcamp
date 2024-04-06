87f6976573262962a164fa4d16916fd96850229c
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-06 21:49:57 +0530

    Refactor --> Added Prime Factors Problem

---
+++ Java DSA BootCamp/Mathematics/src/_2_Prime_Factors.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class _2_Prime_Factors {
+    public static ArrayList<Integer> factor(int n){
+        ArrayList<Integer> ans=new ArrayList<>();
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                ans.add(i);
+                while(n%i==0) n=n/i;
+            }
+        }
+        if(n!=1) ans.add(n);
+        return ans;
+    }
+    public static void main(String[] args) {
+        int n=10;
+        System.out.println(factor(n));
+    }
+}
