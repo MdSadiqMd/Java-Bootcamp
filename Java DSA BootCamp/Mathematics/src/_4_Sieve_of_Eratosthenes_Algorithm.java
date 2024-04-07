56ced592fb34d5b976a2bcad0a4e9cf36170d5cc
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-07 22:17:39 +0530

    Refactor --> Added Seive of Eratoshenes Algorithm

--- Java DSA BootCamp/Mathematics/src/_4_Sieve_of_Eratosthenes_Algorithm.java
+++ Java DSA BootCamp/Mathematics/src/_4_Sieve_of_Eratosthenes_Algorithm.java
@@ -15,7 +15,20 @@ public class _4_Sieve_of_Eratosthenes_Algorithm {
         for(int i=2;i<=n;i++) if(primes[i]==1) ans.add(i);
         return ans;
     }
+    public static ArrayList<Integer> algorithmOptimised(int n){
+        int[] primes=new int[n+1];
+        Arrays.fill(primes,1);
+        ArrayList<Integer> ans=new ArrayList<>();
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(primes[i]==1){
+                for(int j=i*i;j<=n;j+=i) primes[j]=0;
+            }
+        }
+        for(int i=2;i<=n;i++) if(primes[i]==1) ans.add(i);
+        return ans;
+    }
     public static void main(String[] args) {
         System.out.println(algorithm(10));
+        System.out.println(algorithmOptimised(10));
     }
 }
