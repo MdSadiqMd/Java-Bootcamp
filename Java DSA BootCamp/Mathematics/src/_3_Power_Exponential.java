e2d3f064141284555def26f47aab07817ddcd453
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-07 21:51:40 +0530

    Refactor --> Added Power Exponential problem

---
+++ Java DSA BootCamp/Mathematics/src/_3_Power_Exponential.java
@@ -0,0 +1,19 @@
+public class _3_Power_Exponential {
+    public static long pow(int n,int m){
+        long ans=1;
+        while(m>0){
+            if(m%2==1){
+                ans=ans*n;
+                m=m-1;
+            } else {
+                n=n*n;
+                m=m/2;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int n=3,m=5;
+        System.out.println(pow(n,m));
+    }
+}
