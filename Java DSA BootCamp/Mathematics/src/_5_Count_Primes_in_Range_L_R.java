4e08454782434844029fb257a92715729d330b9a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-08 21:12:21 +0530

    Refactor --> Added Count Primes in Range L and R Problem

---
+++ Java DSA BootCamp/Mathematics/src/_5_Count_Primes_in_Range_L_R.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class _5_Count_Primes_in_Range_L_R {
+    public static ArrayList<Integer> helper(int r){
+        int[] primes=new int[r+1];
+        Arrays.fill(primes,1);
+        ArrayList<Integer> ans=new ArrayList<>();
+        for(int i=2;i<=r;i++){
+            if(primes[i]==1){
+                for(int j=2*i;j<=r;j+=i) primes[j]=0;
+            }
+        }
+        for(int i=2;i<=r;i++) if(primes[i]==1) ans.add(i);
+        return ans;
+    }
+    public static int range(int l,int r){ // Using sieve of Eratosthenes
+        int count=0;
+        ArrayList<Integer> primes=helper(r);
+        for(int i=0;i<=primes.size()-1;i++){
+            for(int j=l;j<=r;j++){
+                if(primes.get(i)==1) count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(range(5,10));
+    }
+}
