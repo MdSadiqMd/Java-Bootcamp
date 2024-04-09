19003b20d4b21442ec0547e22705ea127d58c3a3
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-09 14:35:07 +0530

    Refactor --> Added Count Primes in Range L and R using Binary Search

--- Java DSA BootCamp/Mathematics/src/_5_Count_Primes_in_Range_L_R.java
+++ Java DSA BootCamp/Mathematics/src/_5_Count_Primes_in_Range_L_R.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class _5_Count_Primes_in_Range_L_R {
-    public static ArrayList<Integer> helper(int r){
-        int[] primes=new int[r+1];
+    public static ArrayList<Integer> helper(double r){
+        double[] primes=new double[(int) (r+1)];
         Arrays.fill(primes,1);
         ArrayList<Integer> ans=new ArrayList<>();
         for(int i=2;i<=r;i++){
@@ -14,7 +14,8 @@ public class _5_Count_Primes_in_Range_L_R {
         for(int i=2;i<=r;i++) if(primes[i]==1) ans.add(i);
         return ans;
     }
-    public static int range(int l,int r){ // Using sieve of Eratosthenes
+    // Using Sieve of Eratosthenes
+    public static int range(int l,int r){
         int count=0;
         ArrayList<Integer> primes=helper(r);
         for(int i=l;i<=r;i++){
@@ -22,7 +23,22 @@ public class _5_Count_Primes_in_Range_L_R {
         }
         return count;
     }
+    // Using Binary Search
+    public static int rangeOptimised(int l,int r){
+        ArrayList<Integer> primes=helper(r);
+        int low=0,high=primes.size()-1,ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(primes.get(mid)>l){
+                ans=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return primes.size()-ans;
+    }
     public static void main(String[] args) {
-        System.out.println(range(5,10));
+        System.out.println(range(4,10));
+        System.out.println(rangeOptimised(4,10));
     }
 }
