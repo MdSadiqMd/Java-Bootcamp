22b404463f128d1cc9435247f5b770aed8e8a396
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-15 20:04:22 +0530

    Refactor --> Added Number of SubString Consists K Chracteristics

---
+++ Java DSA BootCamp/Sliding Window/src/_6_Number_of_Substrings_Consists_K_Characters.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class _6_Number_of_Substrings_Consists_K_Characters {
+    public static int count(String s){
+        int[] memo={-1,-1,-1};
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            memo[s.charAt(i)-'a']=i;
+            if(memo[0]!=-1 && memo[1]!=-1 && memo[2]!=-1) count+=1+Math.min(memo[0],Math.min(memo[1],memo[2]));
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(count("abcabc"));
+    }
+}
