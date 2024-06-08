a86976321f35dda0308ce9e8f642e0342ad81f57
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-08 21:32:27 +0530

    Refactor --> Added Two SUm with Binary Search

--- Java DSA BootCamp/Blind 75/src/Two_Sum.java
+++ Java DSA BootCamp/Blind 75/src/Two_Sum.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class Two_Sum {
-    public static int[] twoSum(int[] nums, int target) { // TC:O(n),SC:O(n)
+    public static int[] twoSum(int[] nums,int target){ // TC:O(n),SC:O(n)
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<=nums.length-1;i++){
             int search=target-nums[i];
@@ -11,8 +11,20 @@ public class Two_Sum {
         }
         return new int[]{-1,-1};
     }
+    public static int[] twoSumBinary(int[] nums,int target){ // TC:O(N)+O(N*log(N)),SC:O(1)
+        Arrays.sort(nums);
+        int left=0,right=1;
+        for(int i=0;i<=nums.length-1;i++){
+            int search=nums[left]+nums[right];
+            if(search==target) return new int[]{nums[left],nums[right]};
+            else if(search<target) left++;
+            else right--;
+        }
+        return new int[]{-1,-1};
+    }
     public static void main(String[] args) {
         int[] arr={2,6,5,8,11};
         System.out.println(Arrays.toString(twoSum(arr,7)));
+        System.out.println(Arrays.toString(twoSumBinary(arr,7)));
     }
 }
