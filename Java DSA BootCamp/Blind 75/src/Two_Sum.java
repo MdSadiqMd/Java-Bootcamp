6770bae4d9b4a9d0059c55d3ae03c76ec9d0b49d
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-08 21:11:00 +0530

    Refactor --> Added Two Sum Problem using HashMap

---
+++ Java DSA BootCamp/Blind 75/src/Two_Sum.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Two_Sum {
+    public static int[] twoSum(int[] nums, int target) { // TC:O(n),SC:O(n)
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<=nums.length-1;i++){
+            int search=target-nums[i];
+            if(map.containsKey(search)) return new int[]{nums[i],i};
+            map.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
+    }
+    public static void main(String[] args) {
+        int[] arr={2,6,5,8,11};
+        System.out.println(Arrays.toString(twoSum(arr,7)));
+    }
+}
