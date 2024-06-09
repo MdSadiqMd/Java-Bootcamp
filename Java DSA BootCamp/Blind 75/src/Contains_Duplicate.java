9f7fd60fc43b8d9c0a24f85359a6991fb7de22b3
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-09 21:36:26 +0530

    Refactor --> Added Contains Duplicates Problem

--- Java DSA BootCamp/Blind 75/src/Contains_Duplicate.java
+++ Java DSA BootCamp/Blind 75/src/Contains_Duplicate.java
@@ -6,8 +6,10 @@ public class Contains_Duplicate {
     // First think of can we reduce HashMap problem to HashSet rather doing multiple complications
     public static boolean containsDuplicate(int[] nums) { // TC:O(N),SC:O(N)
         HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<=nums.length-1;i++) set.add(nums[i]);
-        return nums.length>set.size();
+        for(int i=0;i<=nums.length-1;i++) {
+            if(!set.add(nums[i])) return true; // If you can't able to add the Integer in set return true
+        };
+        return false;
     }
     // If Arrays problem is Present first think of sorting
     public static boolean containsDuplicateSort(int[] nums){ // TC:O(N*logN),SC:O(1)
