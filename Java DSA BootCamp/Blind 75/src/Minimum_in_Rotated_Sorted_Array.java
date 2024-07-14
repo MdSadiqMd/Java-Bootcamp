df63e9bce3c8514c3189b6a422d2d7c377589866
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-14 21:55:19 +0530

    Refactor --> Added Minimum in Rotated Sorted Array

--- Java DSA BootCamp/Blind 75/src/Minimum_in_Rotated_Sorted_Array.java
+++ Java DSA BootCamp/Blind 75/src/Minimum_in_Rotated_Sorted_Array.java
@@ -1,4 +1,14 @@
 public class Minimum_in_Rotated_Sorted_Array {
+    public static int findMin(int[] arr) {
+        int low=0,high=arr.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[low]<=arr[high]) return arr[low];
+            else if(arr[mid]>arr[high]) low=mid+1;
+            else high=mid;
+        }
+        return arr[low];
+    }
     public static int findMinRecursive(int[] arr,int low,int high) {
         int mid=low+(high-low)/2;
         if(arr[low]<=arr[high]) return arr[low];
@@ -6,6 +16,7 @@ public class Minimum_in_Rotated_Sorted_Array {
         else return findMinRecursive(arr,low,mid);
     }
     public static void main(String[] args) {
+        System.out.println(findMin(new int[]{3,4,5,1,2}));
         System.out.println(findMinRecursive(new int[]{3,4,5,1,2},0,4));
     }
 }
