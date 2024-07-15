5dacd7ff529736d8d9a850a098e35d2e78796355
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-15 18:44:48 +0530

    Refactor --> Added Maximum Product Sub Array Problem

--- Java DSA BootCamp/Blind 75/src/Maximum_Product_SubArray.java
+++ Java DSA BootCamp/Blind 75/src/Maximum_Product_SubArray.java
@@ -4,16 +4,17 @@ public class Maximum_Product_SubArray {
     // +ve, odd -ve -> ans: -ve
     // zero -> ans: zero
     public static int maxProduct(int[] arr) { // If SubArray then think of sliding window(not this question), Manipulate the test cases write all possible test cases and think the approach, Write brute force and start decreasing an order of N in each step
-        int prefix=1,suffix=1;
+        double prefix=1,suffix=1,ans=arr[0]; // Use this rather Integer.MIN_VALUE for this question as nothing had mentioned to return as default value something should be returned by yourselves
         for(int i=0;i<arr.length;i++){
             if(prefix==0) prefix=1; // If prefix becomes 0 then make it as 1
-            prefix=prefix*arr[i];
             if(suffix==0) suffix=1; // If suffix becomes 0 then make it as 1
+            prefix=prefix*arr[i];
             suffix=suffix*arr[arr.length-1-i];
+            ans=Math.max(ans,Math.max(prefix,suffix));
         }
-        return Math.max(prefix,suffix);
+        return (int)ans;
     }
     public static void main(String[] args) {
-        System.out.println(maxProduct(new int[]{1,2,-3,0,-4,-5}));
+        System.out.println(maxProduct(new int[]{0,10,10,10,10,10,10,10,10,10,-10,10,10,10,10,10,10,10,10,10,0}));
     }
 }
