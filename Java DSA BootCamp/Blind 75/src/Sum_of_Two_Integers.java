4fce640ad018d34286a93ebf6a3df960266c6f29
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-16 21:26:27 +0530

    Refactor --> Added Sum of Two Integers

--- Java DSA BootCamp/Blind 75/src/Sum_of_Two_Integers.java
+++ Java DSA BootCamp/Blind 75/src/Sum_of_Two_Integers.java
@@ -1,5 +1,5 @@
 public class Sum_of_Two_Integers {
-    static int add(int a,int b){
+    static int add(int a,int b) { // Think of Carry and check if the number is greater than zero/not equal to zero see in bit manipulation problems the while loop is mostly there
         while(b!=0){
             int carry=a&b;
             a=a^b;
